3a4104703041724a41d77cdcfddd80def860627c
Guillermo Federico Rauch <rauchg@gmail.com>
2009-02-07 16:42:08 -0200

    FormValidator rewritten to be simpler. * FormValidator is more basic. Only deals with validation and event firing logic. Doesn't deal with messages and stuff * Some simple validators included, should be revised * Element.Properties.validator * 'validatedSubmit' event. Use with addEvent() to easily run the validator in a form, and the event will fire if validation is passed

--- Source/Forms/FormValidator.js
+++ Source/Forms/FormValidator.js
@@ -1,456 +1,73 @@
-/*
-Script: FormValidator.js
-	A css-class based form validation system.
-
-	License:
-		MIT-style license.
-
-	Authors:
-		Aaron Newton
-*/
-var InputValidator = new Class({
-
-	Implements: [Options],
-
-	options: {
-		errorMsg: 'Validation failed.',
-		test: function(field){return true;}
-	},
-
-	initialize: function(className, options){
-		this.setOptions(options);
-		this.className = className;
-	},
-
-	test: function(field, props){
-		if ($(field)) return this.options.test($(field), props||this.getProps(field));
-		else return false;
-	},
-
-	getError: function(field, props){
-		var err = this.options.errorMsg;
-		if ($type(err) == "function") err = err($(field), props||this.getProps(field));
-		return err;
-	},
-
-	getProps: function(field){
-		if (!$(field)) return {};
-		return field.get('validatorProps');
-	}
-
-});
-
-Element.Properties.validatorProps = {
-
-	set: function(props){
-		return this.eliminate('validatorProps').store('validatorProps', props);
-	},
-
-	get: function(props){
-		if (props) this.set(props);
-		if (this.retrieve('validatorProps')) return this.retrieve('validatorProps');
-		if (this.getProperty('validatorProps')){
-			try {
-				this.store('validatorProps', JSON.decode(this.getProperty('validatorProps')));
-			}catch(e){
-				return {}
-			}
-		} else {
-			var vals = this.get('class').split(' ').filter(function(cls){
-				return cls.test(':');
-			});
-			if (!vals.length){
-				this.store('validatorProps', {});
-			} else {
-				props = {};
-				vals.each(function(cls){
-					var split = cls.indexOf(':');
-					props[cls.substring(0, split)] = JSON.decode(cls.substring(split+1, cls.length))
-				});
-				this.store('validatorProps', props);
-			}
-		}
-		return this.retrieve('validatorProps');
-	}
-
-};
-
 var FormValidator = new Class({
-
-	Implements:[Options, Events, ToElement],
-	
-	Binds: ['onSubmit'],
-
-	options: {
-		fieldSelectors:"input, select, textarea",
-		ignoreHidden: true,
-		useTitles:false,
-		evaluateOnSubmit:true,
-		evaluateFieldsOnBlur: true,
-		evaluateFieldsOnChange: true,
-		serial: true,
-		stopOnFailure: true,
-		warningPrefix: function(){
-			return FormValidator.resources[FormValidator.language].warningPrefix || 'Warning: ';
-		},
-		errorPrefix: function(){
-			return FormValidator.resources[FormValidator.language].errorPrefix || 'Error: ';
-		}
-//		onFormValidate: function(isValid, form, event){},
-//		onElementValidate: function(isValid, field, className, warn){},
-//		onElementPass: function(field){},
-//		onElementFail: function(field, validatorsFailed){}
-	},
-
-	initialize: function(form, options){
-		this.setOptions(options);
-		this.element = $(form);
-		$(this).store('validator', this);
-		this.warningPrefix = $lambda(this.options.warningPrefix)();
-		this.errorPrefix = $lambda(this.options.errorPrefix)();
-		if (this.options.evaluateOnSubmit) $(this).addEvent('submit', this.onSubmit);
-		if (this.options.evaluateFieldsOnBlur) this.watchFields();
-	},
-
-	toElement: function(){
-		return $(this);
-	},
-
-	getFields: function(){
-		return this.fields = $(this).getElements(this.options.fieldSelectors);
-	},
-
-	watchFields: function(){
-		this.getFields().each(function(el){
-				el.addEvent('blur', this.validateField.pass([el, false], this));
-			if (this.options.evaluateFieldsOnChange)
-				el.addEvent('change', this.validateField.pass([el, true], this));
-		}, this);
-	},
-
-	onSubmit: function(event){
-		if (!this.validate(event) && event) event.preventDefault();
-		else this.reset();
-	},
-
-	reset: function(){
-		this.getFields().each(this.resetField, this);
-		return this;
-	}, 
-
-	validate: function(event){
-		var result = this.getFields().map(function(field){ 
-			return this.validateField(field, true);
-		}, this).every(function(v){ return v;});
-		this.fireEvent('onFormValidate', [result, $(this), event]);
-		if (this.options.stopOnFailure && !result && event) event.preventDefault();
-		return result;
-	},
-
-	validateField: function(field, force){
-		if (this.paused) return true;
-		field = $(field);
-		var passed = !field.hasClass('validation-failed');
-		var failed, warned;
-		if (this.options.serial && !force){
-			failed = $(this).getElement('.validation-failed');
-			warned = $(this).getElement('.warning');
-		}
-		if (field && (!failed || force || field.hasClass('validation-failed') || (failed && !this.options.serial))){
-			var validators = field.className.split(" ").some(function(cn){
-				return this.getValidator(cn);
-			}, this);
-			var validatorsFailed = [];
-			field.className.split(" ").each(function(className){
-				if (!this.test(className,field)) validatorsFailed.include(className);
-			}, this);
-			passed = validatorsFailed.length === 0;
-			if (validators && !field.hasClass('warnOnly')){
-				if (passed){
-					field.addClass('validation-passed').removeClass('validation-failed');
-					this.fireEvent('onElementPass', field);
-				} else {
-					field.addClass('validation-failed').removeClass('validation-passed');
-					this.fireEvent('onElementFail', [field, validatorsFailed]);
-				}
-			}
-			if (!warned){
-				var warnings = field.className.split(" ").some(function(cn){
-					if (cn.test('^warn-') || field.hasClass('warnOnly')) 
-						return this.getValidator(cn.replace(/^warn-/,""));
-					else return null;
-				}, this);
-				field.removeClass('warning');
-				var warnResult = field.className.split(" ").map(function(cn){
-					if (cn.test('^warn-') || field.hasClass('warnOnly')) 
-						return this.test(cn.replace(/^warn-/,""), field, true);
-					else return null;
-				}, this);
-			}
-		}
-		return passed;
-	},
-
-	test: function(className, field, warn){
-		field = $(field);
-		if (field.hasClass('ignoreValidation')) return true;
-		warn = $pick(warn, false);
-		if (field.hasClass('warnOnly')) warn = true;
-		var validator = this.getValidator(className);
-		var isValid = validator.test(field);
-		if (validator && this.isVisible(field)) this.fireEvent('onElementValidate', [isValid, field, className, warn]);
-		if (warn) return true;
-		return isValid;
-	},
-
-	isVisible : function(field){
-		if (!this.options.ignoreHidden) return true;
-		while(field != document.body){
-			if ($(field).getStyle('display') == "none") return false;
-			field = field.getParent();
-		}
-		return true;
-	},
-
-	resetField: function(field){
-		field = $(field);
-		if (field){
-			field.className.split(" ").each(function(className){
-				if (className.test('^warn-')) className = className.replace(/^warn-/,"");
-				field.removeClass('validation-failed');
-				field.removeClass('warning');
-				field.removeClass('validation-passed');
-			}, this);
-		}
-		return this;
-	},
-
-	stop: function(){
-		this.paused = true;
-		return this;
-	},
-
-	start: function(){
-		this.paused = false;
-		return this;
-	},
-
-	ignoreField: function(field, warn){
-		field = $(field);
-		if (field){
-			FormValidator.enforceField(field);
-			if (warn) field.addClass('warnOnly');
-			else field.addClass('ignoreValidation');
-		}
-		return this;
-	},
-
-	enforceField: function(field){
-		field = $(field);
-		if (field) field.removeClass('warnOnly').removeClass('ignoreValidation');
-		return this;
-	}
-
+  
+  Implements: [Options, Events],
+  
+  options: {/*
+    onError: $empty,
+    onErrors: $empty,*/,
+    elementsLimit: null,
+    errorsLimit: null,
+    classPrefix: 'fv_',    
+    fireInputEvent: true
+  },
+  
+  initialize: function(element, options){
+    this.setOptions(options);
+    this.element = $(element);    
+  },
+  
+  run: function(){
+    var r = [];
+    this.element.getElements('input, select, textarea').each(function(input) { 
+      var errors = this.check(input);
+      if(errors){
+        r.push([input, errors]);
+        this.error(input, errors)
+        if(r.length === this.options.elementsLimit) return;
+      }
+    }, this);
+    if(r) this.errors(r);
+    return !! r;
+  },
+  
+  error: function(input, errors){
+    this.fireEvent('error', [input, errors]);
+    if(this.options.fireInputEvent) input.fireEvent('validateError', errors);
+  },
+  
+  errors: function(errors){
+    this.fireEvent('errors', errors);
+  },
+  
+  check: function(input){
+    var errors = [], v = FormValidator.Validators;
+    Hash.each(v, function(value, key){
+      if ((input.hasClass(this.options.classPrefix + key) || input.retrieve(validators, []).contains(key)) && !v[key](value)) 
+        errors.push(key);
+      if (errors.length === this.options.errorsLimit) return;
+    });
+    return errors;
+  }
+  
 });
 
-
-MooTools.lang.addEvent('onLangChange', function(){
-	FormValidator.lang = MooTools.lang.get('FormValidator');
-});
-FormValidator.lang = MooTools.lang.get('FormValidator');
-
-FormValidator.getMsg = function(key){
-	return FormValidator.lang[key];
+FormValidator.Validators = {
+  
+  'valid': function(input){
+    return $chk(input.get('value').clean());
+  },
+  
+  'valid-email': function(input){
+    return input.get('value').clean().test(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  }
+  
 };
-
-FormValidator.adders = {
-
-	validators:{},
-
-	add : function(className, options){
-		this.validators[className] = new InputValidator(className, options);
-		//if this is a class (this method is used by instances of FormValidator and the FormValidator namespace)
-		//extend these validators into it
-		//this allows validators to be global and/or per instance
-		if (!this.initialize){
-			this.implement({
-				validators: this.validators
-			});
-		}
-	},
-
-	addAllThese : function(validators){
-		$A(validators).each(function(validator){
-			this.add(validator[0], validator[1]);
-		}, this);
-	},
-
-	getValidator: function(className){
-		return this.validators[className.split(":")[0]];
-	}
-
-};
-
-$extend(FormValidator, FormValidator.adders);
-
-FormValidator.implement(FormValidator.adders);
-
-FormValidator.add('IsEmpty', {
-
-	errorMsg: false,
-	test: function(element){ 
-		if (element.type == "select-one"||element.type == "select")
-			return !(element.selectedIndex >= 0 && element.options[element.selectedIndex].value != "");
-		else
-			return ((element.get('value') == null) || (element.get('value').length == 0));
-	}
-
-});
-
-FormValidator.addAllThese([
-
-	['required', {
-		errorMsg: function(){
-			return FormValidator.getMsg('required');
-		},
-		test: function(element){ 
-			return !FormValidator.getValidator('IsEmpty').test(element); 
-		}
-	}],
-
-	['minLength', {
-		errorMsg: function(element, props){
-			if ($type(props.minLength))
-				return FormValidator.getMsg('minLength').substitute({minLength:props.minLength,length:element.get('value').length });
-			else return '';
-		}, 
-		test: function(element, props){
-			if ($type(props.minLength)) return (element.get('value').length >= $pick(props.minLength, 0));
-			else return true;
-		}
-	}],
-
-	['maxLength', {
-		errorMsg: function(element, props){
-			//props is {maxLength:10}
-			if ($type(props.maxLength))
-				return FormValidator.getMsg('maxLength').substitute({maxLength:props.maxLength,length:element.get('value').length });
-			else return '';
-		}, 
-		test: function(element, props){
-			//if the value is <= than the maxLength value, element passes test
-			return (element.get('value').length <= $pick(props.maxLength, 10000));
-		}
-	}],
-
-	['validate-integer', {
-		errorMsg: FormValidator.getMsg.pass('integer'),
-		test: function(element){
-			return FormValidator.getValidator('IsEmpty').test(element) || /^-?[1-9]\d*$/.test(element.get('value'));
-		}
-	}],
-
-	['validate-numeric', {
-		errorMsg: FormValidator.getMsg.pass('numeric'), 
-		test: function(element){
-			return FormValidator.getValidator('IsEmpty').test(element) || 
-				/^-?(?:0$0(?=\d*\.)|[1-9]|0)\d*(\.\d+)?$/.test(element.get('value'));
-		}
-	}],
-
-	['validate-digits', {
-		errorMsg: FormValidator.getMsg.pass('digits'), 
-		test: function(element){
-			return FormValidator.getValidator('IsEmpty').test(element) || (/^[\d() .:\-\+#]+$/.test(element.get('value')));
-		}
-	}],
-
-	['validate-alpha', {
-		errorMsg: FormValidator.getMsg.pass('alpha'), 
-		test: function (element){
-			return FormValidator.getValidator('IsEmpty').test(element) ||  /^[a-zA-Z]+$/.test(element.get('value'))
-		}
-	}],
-
-	['validate-alphanum', {
-		errorMsg: FormValidator.getMsg.pass('alphanum'), 
-		test: function(element){
-			return FormValidator.getValidator('IsEmpty').test(element) || !/\W/.test(element.get('value'))
-		}
-	}],
-
-	['validate-date', {
-		errorMsg: function(element, props){
-			if (Date.parse){
-				var format = props.dateFormat || "%x";
-				return FormValidator.getMsg('dateSuchAs').substitute({date:new Date().format(format)});
-			} else {
-				return FormValidator.getMsg('dateInFormatMDY');
-			}
-		},
-		test: function(element, props){
-			if (FormValidator.getValidator('IsEmpty').test(element)) return true;
-			if (Date.parse){
-				var format = props.dateFormat || "%x";
-				var d = Date.parse(element.get('value'));
-				var formatted = d.format(format);
-				if (formatted != "invalid date") element.set('value', formatted);
-				return !isNaN(d);
-			} else {
-			var regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
-			if (!regex.test(element.get('value'))) return false;
-			var d = new Date(element.get('value').replace(regex, '$1/$2/$3'));
-			return (parseInt(RegExp.$1, 10) == (1+d.getMonth())) && 
-			(parseInt(RegExp.$2, 10) == d.getDate()) && 
-			(parseInt(RegExp.$3, 10) == d.getFullYear() );
-			}
-		}
-	}],
-
-	['validate-email', {
-		errorMsg: FormValidator.getMsg.pass('email'), 
-		test: function (element){
-			return FormValidator.getValidator('IsEmpty').test(element) || /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(element.get('value'));
-		}
-	}],
-
-	['validate-url', {
-		errorMsg: FormValidator.getMsg.pass('url'), 
-		test: function (element){
-			return FormValidator.getValidator('IsEmpty').test(element) || /^(https?|ftp|rmtp|mms):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i.test(element.get('value'));
-		}
-	}],
-
-	['validate-currency-dollar', {
-		errorMsg: FormValidator.getMsg.pass('currencyDollar'), 
-		test: function(element){
-			// [$]1[##][,###]+[.##]
-			// [$]1###+[.##]
-			// [$]0.##
-			// [$].##
-			return FormValidator.getValidator('IsEmpty').test(element) ||  /^\$?\-?([1-9]{1}[0-9]{0,2}(\,[0-9]{3})*(\.[0-9]{0,2})?|[1-9]{1}\d*(\.[0-9]{0,2})?|0(\.[0-9]{0,2})?|(\.[0-9]{1,2})?)$/.test(element.get('value'));
-		}
-	}],
-
-	['validate-one-required', {
-		errorMsg: FormValidator.getMsg.pass('oneRequired'), 
-		test: function (element){
-			var p = element.parentNode;
-			return p.getElements('input').some(function(el){
-				if (['checkbox', 'radio'].contains(el.get('type'))) return el.get('checked');
-				return el.get('value');
-			});
-		}
-	}]
-
-]);
 
 Element.Properties.validator = {
-
-	set: function(options){
+  
+  set: function(options){
 		var validator = this.retrieve('validator');
-		if (validator) validator.setOptions(options);
-		return this.store('validator:options');
+		return this.eliminate('validator').store('validator:options', options);
 	},
 
 	get: function(options){
@@ -460,14 +77,35 @@ Element.Properties.validator = {
 		}
 		return this.retrieve('validator');
 	}
+  
+};
 
+Element.Events.validatedSubmit = {
+	base: 'submit',
+	
+	condition: function(e){
+	  if(! this.get('validator').run())
+	  {
+	    new Event(e).stop();
+	    return false;
+	  }
+	  return true;
+	}
 };
 
 Element.implement({
+  
+  validate: function(options){
+    return this.get('validator', options).run();
+  }
+  
+})
 
-	validate: function(options){
-		this.set('validator', options);
-		return this.get('validator', options).validate();
-	}
+// Formvalidator textual extends FormValidator and implements error message reporting, with moo lang.
+// basically extends .error / .errors()
 
-});
+FormValidator.Textual = new Class({
+  
+
+  
+})
