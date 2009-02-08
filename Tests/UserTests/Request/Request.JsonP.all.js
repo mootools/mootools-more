93bd934c49043402463ca5d1f35b913e7337468f
Guillermo Federico Rauch <rauchg@gmail.com>
2009-02-08 13:35:18 -0200

    Request.JsonP tests updated

--- Tests/UserTests/Request/Request.JsonP.all.js
+++ Tests/UserTests/Request/Request.JsonP.all.js
@@ -5,11 +5,12 @@
 			description: "Gets the current rating of an ipod from CNET using script injection",
 			verify: "Did it alert the rating?",
 			before: function(){
-						new Request.JsonP('http://api.cnet.com/restApi/v1.0/techProduct?productId=32069546&iod=none&viewType=json&partKey=19926949750937665684988687810562', {
-				  onComplete: function(data){
-				    alert("The ipod gets an " + data.CNETResponse.TechProduct.EditorsRating.$);
-				  }
-				}).request(); //alerts 8.3 - the rating of the ipod
+				new Request.JsonP({
+				  url: 'http://api.cnet.com/restApi/v1.0/techProduct?productId=32069546&iod=none&viewType=json&partKey=19926949750937665684988687810562',
+		      onComplete: function(data){
+		        alert("The ipod gets an " + data.CNETResponse.TechProduct.EditorsRating.$);
+		      }
+				}).send(); //alerts 8.3 - the rating of the ipod
 			}
 		}
 	]
