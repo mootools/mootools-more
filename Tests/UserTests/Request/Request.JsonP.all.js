1b85798a9ced49352b3831eb5b6c7156e46a7fa1
Guillermo Federico Rauch <rauchg@gmail.com>
2009-02-10 00:05:35 -0200

    Tests updated with JsonP > JSONP

--- Tests/UserTests/Request/Request.JsonP.all.js
+++ Tests/UserTests/Request/Request.JsonP.all.js
@@ -1,11 +1,11 @@
 {
 	tests: [
 		{
-			title: "Request.JsonP",
+			title: "Request.JSONP",
 			description: "Gets the current rating of an ipod from CNET using script injection",
 			verify: "Did it alert the rating?",
 			before: function(){
-				new Request.JsonP({
+				new Request.JSONP({
 				  url: 'http://api.cnet.com/restApi/v1.0/techProduct?productId=32069546&iod=none&viewType=json&partKey=19926949750937665684988687810562',
 		      onComplete: function(data){
 		        alert("The ipod gets an " + data.CNETResponse.TechProduct.EditorsRating.$);
