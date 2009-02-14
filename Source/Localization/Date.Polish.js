b2468bc07a1a61894c390e7103287cc1360a943d
Oskar Krawczyk <oskar.krawczyk@gmail.com>
2009-02-14 14:12:43 +0000

    Added date localizations for Polish.

---
+++ Source/Localization/Date.Polish.js
@@ -0,0 +1,38 @@
+/*
+Script: Date.Polish.js
+	Date messages for Polish. Thanks Oskar Krawczyk.
+
+	License:
+		MIT-style license.
+
+*/
+
+MooTools.lang.set('PL', 'Date', {
+
+	months: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+	days: ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'],
+	dateOrder: ['month', 'date', 'year', '/'],
+	AM: 'nad ranem',
+	PM: 'po południu',
+
+	/* Date.Extras */
+	getOrdinal: function(dayOfMonth){
+		return (dayOfMonth > 3 && dayOfMonth < 21) ? 'ty' : ['ty', 'szy', 'gi', 'ci', 'ty'][Math.min(dayOfMonth % 10, 4)];
+	},
+
+	lessThanMinuteAgo: 'mniej niż minute temu',
+	minuteAgo: 'około minutę temu',
+	minutesAgo: '{delta} minut temu',
+	hourAgo: 'około godzinę temu',
+	hoursAgo: 'około {delta} godzin temu',
+	dayAgo: '1 dzień temu',
+	daysAgo: '{delta} dni temu',
+	lessThanMinuteUntil: 'za niecałą minutę',
+	minuteUntil: 'za około minutę',
+	minutesUntil: 'za {delta} minut',
+	hourUntil: 'za około godzinę',
+	hoursUntil: 'za około {delta} godzin',
+	dayUntil: 'za 1 dzień',
+	daysUntil: 'za {delta} dni'
+
+});
