bfef7903c6ebf4710be8fc3ea474a3d5820cb98a
anutron <git@clientcide.com>
2009-02-15 09:19:41 -0800

    adding a few inline comments to date js

--- Source/Localization/Date.English.US.js
+++ Source/Localization/Date.English.US.js
@@ -11,12 +11,14 @@ MooTools.lang.set('usENG', 'Date', {
 
 	months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
 	days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	//culture's date order: MM/DD/YYYY
 	dateOrder: ['month', 'date', 'year', '/'],
 	AM: 'AM',
 	PM: 'PM',
 
 	/* Date.Extras */
 	getOrdinal: function(dayOfMonth){
+		//1st, 2nd, 3rd, etc.
 		return (dayOfMonth > 3 && dayOfMonth < 21) ? 'th' : ['th', 'st', 'nd', 'rd', 'th'][Math.min(dayOfMonth % 10, 4)];
 	},
 
