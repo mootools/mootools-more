0f6b9767a551c131ca230f40cf8720fa29ab403e
nsorosac <nicolas.sorosac@gmail.com>
2009-02-16 01:08:41 -0700

    FormValidator.Extras included with ASCII characters

--- Source/Localization/FormValidator.French.js
+++ Source/Localization/FormValidator.French.js
@@ -1,29 +1,45 @@
 /*
 Script: FormValidator.French.js
-	FormValidator messages in French. Thanks Miquel Hudin.
+  FormValidator messages in French.
 
-	License:
-		MIT-style license.
+Author: 
+  Nicolas Sorosac <nicolas.sorosac@gmail.com>
 
+License:
+  http://www.clientcide.com/wiki/cnet-libraries#license
 */
-
+ 
 MooTools.lang.set('FR', 'FormValidator', {
-
-	required:'Ce champ est obligatoire.',
-	minLength:'S\'il vous vous plat crivez au moins {minLength} caractres (vous avez crit {length} caractres).',
-	maxLength:'S\'il vous vous plat n\'crivez pas plus de {maxLength} caractres (vous avez crit {length} caractres).',
-	integer:'S\'il vous vous plat crivez un nombre entier dans ce champ. Les nombres avec des dcimals  (p.ex. 1\'25) ne sont pas permis.',
-	numeric:'S\'il vous vous plat crivez seulement des chiffres dans ce champ (p.ex. "1" ou "1\'1" ou "-1" or "-1\'1").',
-	digits:'S\'il vous vous plat crivez des nombres et des signes de ponctuation seulement dans ce champ (par exemple, un numro de tlphone avec des traits d\'union est permis).',
-	alpha:'S\'il vous vous plat crivez seulement des lettres (a-z) dans ce champ. Les espaces ou d\'autres caractres ne sont pas permis.',
-	alphanum:'S\'il vous vous plat crivez seulement des lettres (a-z) ou des chiffres (0-9) dans ce champ. Les espaces ou d\'autres caractres ne sont pas permis.',
-	dateSuchAs:'S\'il vous vous plat crivez une date correcte, comme {date}',
-	dateInFormatMDY:'S\'il vous vous plat crivez une date correcte, comme JJ/MM/AAAA (p.ex. "31/11/1999")',
-	email:'S\'il vous vous plat crivez une adresse de courrier lectronique. Par example "fred@domain.com".',
-	url:'S\'il vous vous plat crivez une URL, comme http://www.google.com.',
-	currencyDollar:'S\'il vous vous plat crivez une quantit correcte. Par example $100.00 .',
-	oneRequired:'S\'il vous vous plat slectionnez au moins une de ces options.',
-	errorPrefix: 'Erreur: ',
-	warningPrefix: 'Attention: '
-
+  required:'Ce champ est obligatoire.',
+  minLength:'Veuillez saisir un minimum de {minLength} caract&egrave;re(s) (vous avez saisi {length} caract&egrave;re(s)).',
+  maxLength:'Veuillez saisir un maximum de {maxLength} caract&egrave;re(s) (vous avez saisi {length} caract&egrave;re(s)).',
+  integer:'Veuillez saisir un nombre entier dans ce champ. Les nombres d&eacute;cimaux (ex : "1,25") ne sont pas autoris&eacute;s.',
+  numeric:'Veuillez saisir uniquement des chiffres dans ce champ (ex : "1" ou "1,1" ou "-1" ou "-1,1").',
+  digits:'Veuillez saisir uniquement des chiffres et des signes de ponctuation dans ce champ (ex : un num&eacute;ro de t&eacute;l&eacute;phone avec des traits d\'union est autoris&eacute;).',
+  alpha:'Veuillez saisir uniquement des lettres (a-z) dans ce champ. Les espaces ou autres caract&egrave;res ne sont pas autoris&eacute;s.',
+  alphanum:'Veuillez saisir uniquement des lettres (a-z) ou des chiffres (0-9) dans ce champ. Les espaces ou autres caract&egrave;res ne sont pas autoris&eacute;s.',
+  dateSuchAs:'Veuillez saisir une date correcte comme {date}',
+  dateInFormatMDY:'Veuillez saisir une date correcte, au format JJ/MM/AAAA (ex : "31/11/1999").',
+  email:'Veuillez saisir une adresse de courrier &eacute;lectronique. Par example "fred@domaine.com".',
+  url:'Veuillez saisir une URL, comme http://www.google.com.',
+  currencyDollar:'Veuillez saisir une quantit&eacute; correcte. Par example 100,00&euro;.',
+  oneRequired:'Veuillez s&eacute;lectionner au moins une de ces options.',
+  errorPrefix: 'Erreur : ',
+  warningPrefix: 'Attention : '
+  
+  //FormValidator.Extras
+ 
+  noSpace: 'Ce champ n\'accepte pas les espaces.',
+  reqChkByNode: 'Aucun &eacute;l&eacute;ment n\'est s&eacute;lectionn&eacute;.',
+  requiredChk: 'Ce champ est obligatoire.',
+  reqChkByName: 'Veuillez s&eacute;lectionner un(e) {label}.',
+  match: 'Ce champ doit correspondre avec le champ {matchName}.',
+  startDate: 'date de d&eacute;but',
+  endDate: 'date de fin',
+  currendDate: 'date actuelle',
+  afterDate: 'La date doit &ecirc;tre identique ou post&eacute;rieure &agrave; {label}.',
+  beforeDate: 'La date doit &ecirc;tre identique ou ant&eacute;rieure &agrave; {label}.',
+  startMonth: 'Veuillez s&eacute;lectionner un mois de d&eacute;but.',
+  sameMonth: 'Ces deux dates doivent &ecirc;tre dans le m&ecirc;me mois - vous devez en modifier une.'
+ 
 });
