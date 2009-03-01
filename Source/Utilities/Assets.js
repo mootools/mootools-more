8d1242c657143298b21ed1fd9b84310a7c61cc8b
Guillermo Federico Rauch <rauchg@gmail.com>
2009-03-01 21:02:47 -0200

    Assets revamp concept

--- Source/Utilities/Assets.js
+++ Source/Utilities/Assets.js
@@ -7,56 +7,104 @@ Script: Assets.js
 
 	Authors:
 		Valerio Proietti
+		Guillermo Rauch
 */
 
-var Asset = {
+var Asset = new Class({
+	
+	Implements: [Options, Events],
+	
+	options: {/*
+		onLoad: $empty,
+		onLoaded: $empty,
+		onComplete: $empty,*/
+		properties: {},
+		document: document
+	},
+	
+	loaded: function(){
+		this.fireEvent('complete', this.element).fireEvent('loaded', this.element);
+	}
+	
+});
 
-	javascript: function(source, properties){
-		properties = $extend({
-			onload: $empty,
-			document: document,
-			check: $lambda(true)
-		}, properties);
-
-		var script = new Element('script', {src: source, type: 'text/javascript'});
-
-		var load = properties.onload.bind(script), check = properties.check, doc = properties.document;
-		delete properties.onload; delete properties.check; delete properties.document;
-
-		script.addEvents({
-			load: load,
+Asset.Javascript = new Class({
+	
+	Extends: Asset,
+	
+	options: {		
+		check: $lambda(true)
+	},
+	
+	initialize: function(url, options){
+		this.setOptions(options);
+		this.url = url;
+		
+		var load = this.loaded.bind(this), check = this.options.check;
+		this.element = new Element('script', $merge(this.options.properties, {src: this.url, type: 'text/javascript'})).addEvents({
+			load: loaded,
 			readystatechange: function(){
-				if (['loaded', 'complete'].contains(this.readyState)) load();
+				if (['loaded', 'complete'].contains(this.readyState)) loaded();
 			}
-		}).set(properties);
+		});
 
 		if (Browser.Engine.webkit419) var checker = (function(){
+			if (!$try(check)) return;
+			$clear(checker);
+			loaded();
+		}).periodical(50);
+
+		this.element.inject(this.options.document.head);
+		this.fireEvent('load');
+	}
+	
+});
+
+Asset.Stylesheet = new Class({
+	
+	Extends: Asset,
+		
+	initialize: function(url, options){
+		this.setOptions(options);
+		this.url = url;
+		
+		this.element = new Element('link', $merge({
+			rel: 'stylesheet', media: 'screen', type: 'text/css', href: source
+		}, this.options.properties)).inject(this.document.head);
+		
+		// don't know if this method will work 
+		// otherwise we can ask for a "check: selector" option and use Fx.CSS.search periodically
+		// or we can load it through Request
+		var check = function(){
+			this.options.document.styleSheets.each(function(style){
+				if(style.href && style.href = this.url) this.loaded();
+			}, this);
+		}.bind(this);
+		
+		var checker = (function(){
 			if (!$try(check)) return;
 			$clear(checker);
 			load();
 		}).periodical(50);
 
-		return script.inject(doc.head);
-	},
+		this.fireEvent('load');
+	}
+	
+});
 
-	css: function(source, properties){
-		return new Element('link', $merge({
-			rel: 'stylesheet', media: 'screen', type: 'text/css', href: source
-		}, properties)).inject(document.head);
-	},
-
-	image: function(source, properties){
-		properties = $merge({
-			onload: $empty,
-			onabort: $empty,
-			onerror: $empty
-		}, properties);
+Asset.Image = new Class({
+	
+	Extends: Asset,
+	
+	initialize: function(url, options){
+		this.setOptions(options);
+		this.url = url;
+		
 		var image = new Image();
 		var element = $(image) || new Element('img');
+		
 		['load', 'abort', 'error'].each(function(name){
 			var type = 'on' + name;
-			var event = properties[type];
-			delete properties[type];
 			image[type] = function(){
 				if (!image) return;
 				if (!element.parentNode){
@@ -64,32 +112,43 @@ var Asset = {
 					element.height = image.height;
 				}
 				image = image.onload = image.onabort = image.onerror = null;
-				event.delay(1, element, element);
+				this.loaded.delay(1, this);
 				element.fireEvent(name, element, 1);
 			};
-		});
-		image.src = element.src = source;
-		if (image && image.complete) image.onload.delay(1);
-		return element.set(properties);
-	},
-
-	images: function(sources, options){
-		options = $merge({
-			onComplete: $empty,
-			onProgress: $empty
-		}, options);
-		sources = $splat(sources);
-		var images = [];
-		var counter = 0;
-		return new Elements(sources.map(function(source){
-			return Asset.image(source, {
-				onload: function(){
-					options.onProgress.call(this, counter, sources.indexOf(source));
-					counter++;
-					if (counter == sources.length) options.onComplete();
-				}
-			});
-		}));
+		}, this);
+		
+		image.src = element.src = this.url;
+		if (image && image.complete) image.onload.delay(1);		
+		
+		this.element = element.set(this.options.properties);
+		this.fireEvent('load');		
 	}
+	
+});
 
-};
+Asset.Images = new Class({
+	
+	Extends: Asset,
+	
+	initialize: function(urls, options){
+		this.setOptions(options);
+		this.urls = urls;
+		this.elements = [];
+		
+		var instances = [];
+		this.urls.each(function(url){
+			var image = new Asset.Image(url, this.options);
+			instances.push(image);
+			this.elements.push[image.element];
+			this.fireEvent('load', url);
+		}, this);		
+		
+		this.group = new Group(instances);
+		this.group.addEvent('complete', this.loaded.bind(this));
+	},
+	
+	loaded: function(){
+		this.fireEvent('complete', this.elements).fireEvent('loaded', this.elements);
+	}
+	
+});
