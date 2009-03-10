4b7ba6c8c5aa5dd51da38e52fe494f2e294a4c4b
Aaron Newton <git@clientcide.com>
2009-03-10 10:21:04 -0700

    JSONP no longer uses indexes for it's callbacks - this makes flickr work; closing issue #12

--- Tests/UserTests/Request/Request.JSONP.all.js
+++ Tests/UserTests/Request/Request.JSONP.all.js
@@ -12,6 +12,24 @@
 					}
 				}).send(); //alerts 8.3 - the rating of the ipod
 			}
+		},
+		{
+			title: "Request.JSONP",
+			description: "Gets an image from flickr",
+			verify: "Did it load an image?",
+			before: function(){
+				new Request.JSONP({
+					callBackKey: 'jsoncallback',
+					url: 'http://www.flickr.com/services/feeds/photos_public.gne?format=json',
+					onComplete: function(data){
+						new Element('div').adopt(
+							new Element('img', {
+								src: data.items[0].media.m
+							})
+						).inject(document.body);
+					}
+				}).send(); //alerts 8.3 - the rating of the ipod
+			}
 		}
 	]
 }
