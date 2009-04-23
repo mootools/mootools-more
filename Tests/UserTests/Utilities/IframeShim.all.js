365f961bd5022c214387b2ac484023de284a3a8c
anutron <git@clientcide.com>
2009-04-23 00:01:50 -0700

    updating iframeshim test

--- Tests/UserTests/Utilities/IframeShim.all.js
+++ Tests/UserTests/Utilities/IframeShim.all.js
@@ -4,7 +4,11 @@
 			title: "Fix the select box - IE6 only!",
 			description: "Fix the select list that pops through the box.",
 			verify: "Did the select list get obscured by the box?",
-			body: "new IframeShim('foo', {display: true});"
+			before: function(){
+				IframeShim.ready = true;
+				new IframeShim('foo', {display: true});
+				window.fireEvent('load')
+			}
 		}
 	]
 }
