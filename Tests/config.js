bf9a3928e2d87b5a94b8a30eb2eab29da48a6bf6
Aaron Newton <git@clientcide.com>
2009-06-11 16:56:19 -0700

    updating unit tester framework

--- Tests/config.js
+++ Tests/config.js
@@ -1,39 +1,14 @@
-UnitTester.site = 'MooTools More';
-UnitTester.title = 'Unit Tests';
-
-window.addEvent('load', function(){
-	var sources = {
-		mootoolsCore: '../assets/mootools-core',
-		mootoolsMore: '../'
-	};
-
-	new UnitTester(sources, {
-		'mootools-more': 'UserTests/'
-	}, {
-		autoplay: true
-	});
-});
-
-
 var UnitTester = {
-	site: 'Clientcide',
+	site: 'MooTools More',
 	title: 'Unit Tests',
 	path: '../UnitTester/',
 	ready: function(){
 		var sources = {
-			mootoolsCore: '../../core',
-			mootoolsMore: '../../more',
-			clientcide: '..'
+			mootoolsCore: '../assets/mootools-core',
+			mootoolsMore: '..'
 		};
-		if (window.location.href.contains("http://www.clientcide.com/tests")) {
-			sources = {
-				mootoolsCore: '/cnet.gf/Mootools/mootools-core',
-				mootoolsMore: '/cnet.gf/Mootools/mootools-more',
-				clentcide: '/cnet.gf/svn'
-			};
-		}
 		new UnitTester(sources, {
-			Clientcide: 'UserTests/'
+			'mootools-more': 'UserTests'
 		}, {
 			autoplay: true
 		});
