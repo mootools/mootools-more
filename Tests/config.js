a440ecd48d3c4605e4daac1914bb910efda8ea9c
Aaron Newton <git@clientcide.com>
2009-06-11 16:28:21 -0700

    updating lang to be a submodule, updating test framework config

--- Tests/config.js
+++ Tests/config.js
@@ -13,3 +13,29 @@ window.addEvent('load', function(){
 		autoplay: true
 	});
 });
+
+
+var UnitTester = {
+	site: 'Clientcide',
+	title: 'Unit Tests',
+	path: '../UnitTester/',
+	ready: function(){
+		var sources = {
+			mootoolsCore: '../../core',
+			mootoolsMore: '../../more',
+			clientcide: '..'
+		};
+		if (window.location.href.contains("http://www.clientcide.com/tests")) {
+			sources = {
+				mootoolsCore: '/cnet.gf/Mootools/mootools-core',
+				mootoolsMore: '/cnet.gf/Mootools/mootools-more',
+				clentcide: '/cnet.gf/svn'
+			};
+		}
+		new UnitTester(sources, {
+			Clientcide: 'UserTests/'
+		}, {
+			autoplay: true
+		});
+	}
+};
