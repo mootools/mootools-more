bb336f0f4ae38b22ea8d3c03e754ad78f47f44d6
Aaron Newton <git@clientcide.com>
2009-06-11 17:25:16 -0700

    moving UnitTester into /Tests

--- Tests/config.js
+++ Tests/config.js
@@ -1,7 +1,7 @@
 var UnitTester = {
 	site: 'MooTools More',
 	title: 'Unit Tests',
-	path: '../UnitTester/',
+	path: 'UnitTester/',
 	ready: function(){
 		var sources = {
 			mootoolsCore: '../assets/mootools-core',
