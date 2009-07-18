73bbf5171286fd86cf2ebf7c59bd845b438a7420
Aaron Newton <git@clientcide.com>
2009-07-18 00:06:21 -0700

    adding Elements.From w/ tests and such

---
+++ Source/Element/Elements.From.js
@@ -0,0 +1,21 @@
+/*
+Script: Elements.From.js
+	Returns a collection of elements from a string of html.
+
+	License:
+		MIT-style license.
+
+	Authors:
+		Aaron Newton
+
+*/
+Elements.from = function(text) {
+	var container;
+	if (text.match(/^\<(td|tr|th)/)) {
+		var table = new Element('table');
+		container = new Element('tbody').inject(table);
+	} else {
+		container = new Element('div');
+	}
+	return container.set('html', text).getChildren();
+};
