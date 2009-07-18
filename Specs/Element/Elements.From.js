9741c65a05f05fcd0070443456843771c56fbe41
Aaron Newton <git@clientcide.com>
2009-07-18 00:06:21 -0700

    adding Elements.From w/ tests and such

---
+++ Specs/Element/Elements.From.js
@@ -0,0 +1,15 @@
+describe('Elements.From', {
+
+	'should return a group of elements': function(){
+		var str = '<p><b>foo</b></p><i>bar</i>';
+		var div = new Element('div');
+		value_of(div.adopt(Elements.from(str)).get('html')).should_be(str);
+	},
+	
+	'should return a group of table elements': function(){
+		var str = '<tr><td>foo</td></tr>';
+		var tbody = new Element('tbody').inject(new Element('table')).adopt(Elements.from(str));
+		value_of(tbody.get('html')).should_be(str);
+	}
+
+});
