efa05d5a850f50a6990248949096bf2502f8a476
Christoph Pojer <christoph.pojer@gmail.com>
2009-08-26 19:49:38 +0200

    * More sophisticated check if an element is in the DOM if for some reason it is moved to another place in the dom where the element and the overtext element don't share the same parent (rarely occurs though). Added a test for this.

--- Source/Forms/OverText.js
+++ Source/Forms/OverText.js
@@ -136,7 +136,7 @@ var OverText = new Class({
 
 	reposition: function(){
 		this.assert(true);
-		if (!this.element.getParent() || !this.element.offsetHeight) return this.stopPolling().hide();
+		if (!document.body.hasChild(this.element) || !this.element.offsetHeight) return this.stopPolling().hide();
 		if (this.test()) this.text.position($merge(this.options.positionOptions, {relativeTo: this.element}));
 		return this;
 	}
