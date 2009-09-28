49270509cefcb113a5cd06315f7d35efca0677ca
Aaron Newton <git@clientcide.com>
2009-09-28 11:12:53 -0700

    moving some startup logic into it's own method

--- Source/Interface/Keyboard.js
+++ Source/Interface/Keyboard.js
@@ -61,9 +61,12 @@ Script: Keyboard.js
 
 		initialize: function(options){
 			this.setOptions(options);
-			this.addEvents(this.options.events);
 			//if this is the root manager, nothing manages it
 			if (Keyboard.manager) Keyboard.manager.manage(this);
+		},
+
+		setup: function(){
+			this.addEvents(this.options.events);
 			if (this.options.active) this.activate();
 		},
 
