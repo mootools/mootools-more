1c7e13b58229a03c3c8cc32f23dd15c623c8261a
Aaron Newton <git@clientcide.com>
2009-10-01 17:46:13 -0700

    handling the case where the document body is shorter than the window

--- Source/Interface/Mask.js
+++ Source/Interface/Mask.js
@@ -80,6 +80,7 @@ var Mask = new Class({
 		this.resize(this.options.width, this.options.height);
 		this.element.position({
 			relativeTo: this.target,
+			position: 'topLeft',
 			ignoreMargins: !this.options.maskMargins,
 			ignoreScroll: this.target == document.body
 		});
@@ -92,9 +93,14 @@ var Mask = new Class({
 		};
 		if (this.options.maskMargins) opt.styles.push('margin');
 		var dim = this.target.getComputedSize(opt);
+		if (this.target == document.body) {
+			var win = window.getSize();
+			if (dim.totalHeight < win.y) dim.totalHeight = win.y;
+			if (dim.totalWidth < win.x) dim.totalWidth = win.x;
+		}
 		this.element.setStyles({
-			width:($pick(x, dim.totalWidth)),
-			height:($pick(y, dim.totalHeight))
+			width: $pick(x, dim.totalWidth, dim.x),
+			height: $pick(y, dim.totalHeight, dim.y)
 		});
 		return this;
 	},
