2326ed4f6994f46f14a35cb447fd34f9e7ee8896
Nathan White <nw@nwhite.net>
2009-10-08 17:04:00 -0700

    Added tested case for ticket #124 (addSection in Accordion) can't replicate

--- Tests/UserTests/Fx/Fx.Accordion.all.js
+++ Tests/UserTests/Fx/Fx.Accordion.all.js
@@ -17,6 +17,19 @@
 					trigger: 'mouseenter'
 				});
 			}
+		},
+		{
+			title: "Fx.Accordion",
+			description: "Dynamically add sections to an existing accordion.",
+			verify: "Is there a 'fourth section' does it respond like the others?",
+			before : function(){
+				var accordion = new Fx.Accordion($$('dt'), $$('dd'));
+				var container = $('accordionExample').getElement('dl')
+				accordion.addSection(
+					new Element('dt',{'class': 'toggle', 'html' : '<b>fourth section</b>'}).inject(container),
+					new Element('dd',{'class': 'stretcher', 'html' : "I'n the content for the fourth section."}).inject(container)
+				);
+			}
 		}
 	]
 }
