854443dc3773cd701e0050a415a1e1518b049c6a
Aaron Newton <git@clientcide.com>
2009-10-20 17:30:12 -0700

    fixing https://mootools.lighthouseapp.com/projects/24057/tickets/145

--- Tests/UserTests/Forms/Form.Validator.Inline.all.js
+++ Tests/UserTests/Forms/Form.Validator.Inline.all.js
@@ -8,8 +8,24 @@
 				new Form.Validator.Inline('foo', {
 					serial: false,
 					onFormValidate: function(passed, form, event){
-						//if (passed) alert('form validated');
-						//else alert('form did NOT validate');
+						event.stop();
+						if (passed) alert('form validated');
+						else alert('form did NOT validate');
+					}
+				});
+			}
+		},
+		{
+			title: "Form.Validator: tests a single input",
+			description: "Validates that a form's input is correct.",
+			verify: "1) focus the input, tab out - do you see the error? 2) add a value, ",
+			before: function(){
+				new Form.Validator.Inline('foo2', {
+					serial: false,
+					onFormValidate: function(passed, form, event){
+						event.stop();
+						if (passed) alert('form validated');
+						else alert('form did NOT validate');
 					}
 				});
 			}
