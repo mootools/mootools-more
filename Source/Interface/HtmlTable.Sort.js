990cb5c6b23ae8f812a229f1e7bbcfe6e687e090
Aaron Newton <git@clientcide.com>
2009-10-20 17:49:45 -0700

    fixing darren's api error for Date.format

--- Source/Interface/HtmlTable.Sort.js
+++ Source/Interface/HtmlTable.Sort.js
@@ -238,7 +238,7 @@ HtmlTable.Parsers = new Hash({
 	'date': {
 		match: /^\d{2}[-\/ ]\d{2}[-\/ ]\d{2,4}$/,
 		convert: function() {
-			return Date.parse(this.get('text').get('db'));
+			return Date.parse(this.get('text').format('db'));
 		},
 		type: 'date'
 	},
