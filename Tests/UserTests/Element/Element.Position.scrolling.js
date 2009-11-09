bb8797e8b7dc666683e5324ec5bfe738d86ea7bb
Ian Collins <ian.collins@gmail.com>
2009-11-09 14:07:44 -0800

    more robust test for Element.Position including a fixed position containing element.

--- Tests/UserTests/Element/Element.Position.scrolling.js
+++ Tests/UserTests/Element/Element.Position.scrolling.js
@@ -3,20 +3,23 @@
 		{
 			title: "Scrolling and Element.Position",
 			description: "Makes the red & yellow boxes follow the green box during scroll",
-			verify: "Do the red and yellow boxes follow the green box when you scroll either scrollbar?",
+			verify: "Do the red and yellow boxes follow the green box when you scroll any scrollbar?",
 			before: function(){
-			  var red = $('red'),
-            green = $('green'),
-            yellow = $('yellow');
+			  var red1 = $('red1'), red2 = $('red2'),
+            green1 = $('green1'), green2 = $('green2'),
+            yellow1 = $('yellow1'), yellow2 = $('yellow2');
 
         var position = function(){
-          green.position({relativeTo: red, position: 'centerLeft', edge: 'centerRight'});
-				  yellow.position({relativeTo: red, position: 'centerRight', edge: 'centerLeft'});
+          green1.position({relativeTo: red1, position: 'centerLeft', edge: 'centerRight'});
+				  yellow1.position({relativeTo: red1, position: 'centerRight', edge: 'centerLeft'});
+				  green2.position({relativeTo: red2, position: 'centerLeft', edge: 'centerRight'});
+				  yellow2.position({relativeTo: red2, position: 'centerRight', edge: 'centerLeft'});
         };
         
         position();        
         window.addEvent('scroll', position);
-        $('inner-container').addEvent('scroll', position);
+        $('inner-container1').addEvent('scroll', position);
+        $('inner-container2').addEvent('scroll', position);
 			}
 		}
 	],
