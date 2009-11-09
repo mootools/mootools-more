9ba6c90acb3acf3ec5688840ed01b1a94746f8ad
Ian Collins <ian.collins@gmail.com>
2009-11-09 15:05:04 -0800

    another set of tiles to position for Element.Position scrolling test

--- Tests/UserTests/Element/Element.Position.scrolling.js
+++ Tests/UserTests/Element/Element.Position.scrolling.js
@@ -5,15 +5,11 @@
 			description: "Makes the red & yellow boxes follow the green box during scroll",
 			verify: "Do the red and yellow boxes follow the green box when you scroll any scrollbar?",
 			before: function(){
-			  var red1 = $('red1'), red2 = $('red2'),
-            green1 = $('green1'), green2 = $('green2'),
-            yellow1 = $('yellow1'), yellow2 = $('yellow2');
-
         var position = function(){
-          green1.position( {relativeTo: red1, position: 'centerLeft', edge: 'centerRight'});
-				  yellow1.position({relativeTo: red1, position: 'centerRight', edge: 'centerLeft'});
-				  green2.position( {relativeTo: red2, position: 'centerLeft', edge: 'centerRight'});
-				  yellow2.position({relativeTo: red2, position: 'centerRight', edge: 'centerLeft'});
+          ['0','1','2'].each(function(post){
+            $('green' + post).position( {relativeTo: $('red' + post), position: 'centerRight', edge: 'centerLeft'});
+            $('yellow' + post).position({relativeTo: $('red' + post), position: 'centerLeft', edge: 'centerRight'});
+          });
         };
         
         position();        
