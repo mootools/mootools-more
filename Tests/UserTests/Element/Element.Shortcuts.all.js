4e6436f58a171dded4f044b15e679c4ed6e92f67
Aaron Newton <git@clientcide.com>
2009-12-01 17:09:41 -0800

    reworking hide/show logic w/ scott's help.

--- Tests/UserTests/Element/Element.Shortcuts.all.js
+++ Tests/UserTests/Element/Element.Shortcuts.all.js
@@ -6,33 +6,41 @@
 			verify: "Did the element disappear?",
 			before: function(){$('foo').setStyle('display','block');},
 			post: function(){return $('foo').getStyle('display') == 'none';},
-			body: "$('foo').hide();"
+			before: function(){
+				$('foo').hide();
+			}
 		},
 		{
 			title: "Element.show",
 			description: "Switches an element from display:none to display:block",
 			verify: "Did the element appear?",
-			before: function(){$('foo').setStyle('display','none');},
-			post: function(){return $('foo').getStyle('display') == 'block';},
-			body: "$('foo').show();"
+			before: function(){
+				$('foo').setStyle('display','none');
+				$('foo').show();
+			},
+			post: function(){return $('foo').getStyle('display') == 'block';}
 		},
 		{
 			title: "Element.show(type)",
 			description: "Switches an element from display:none to a different, specified display.",
 			verify: "Did the element appear inline?",
-			before: function(){$('foo').hide();},
-			post: function(){return $('foo').getStyle('display') == 'inline';},
-			body: "$('foo').show('inline');"
+			before: function(){
+				$('foo').hide();
+				$('foo').show('inline');
+			},
+			post: function(){return $('foo').getStyle('display') == 'inline';}
 		},
 		{
 			title: "Element.toggle",
 			description: "Toggles between display:none and block.",
 			verify: "Did the element switch between hidden and visible?",
-			before: function(){$('foo').show();},
-			body: "$('foo').toggle();\n\
-	$('foo').toggle.delay(500, $('foo'));\n\
-	$('foo').toggle.delay(1000, $('foo'));\n\
-	$('foo').toggle.delay(1500, $('foo'));"
+			before: function(){
+				$('foo').show();
+				$('foo').toggle();
+				$('foo').toggle.delay(500, $('foo'));
+				$('foo').toggle.delay(1000, $('foo'));
+				$('foo').toggle.delay(1500, $('foo'));
+			}
 		},
 		{
 			title: "Element.isDisplayed",
