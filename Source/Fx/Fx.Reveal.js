cf3cb8211a493909fe04c9bd117444d27c41bff7
Aaron Newton <git@clientcide.com>
2010-01-27 22:35:35 -0800

    Fx.Reveal; fixes #190

--- Source/Fx/Fx.Reveal.js
+++ Source/Fx/Fx.Reveal.js
@@ -50,7 +50,7 @@ Fx.Reveal = new Class({
 						styles: this.options.styles,
 						mode: this.options.mode
 					});
-					this.element.setStyle('display', 'block');
+					this.element.setStyle('display', this.options.display);
 					if (this.options.transitionOpacity) startStyles.opacity = 1;
 					var zero = {};
 					$each(startStyles, function(style, name){
