48c0749a07b9f9912e8a24187379a1b303c6158b
Daniel Buchner <danieljb2@gmail.com>
2010-01-30 11:11:58 -0700

    Delegation fixed to support correct mouseenter/leave behavior, while also preserving native mouseover/out delegation abilities.
    A rework should be done to catch the rest of the cases like focus, blur, etc... - I could think more about that an try to do it if you all think I should.

--- Source/Element/Element.Delegation.js
+++ Source/Element/Element.Delegation.js
@@ -24,7 +24,7 @@ provides: [Element.Delegation]
 ...
 */
 
-(function(addEvent, removeEvent){
+(function(){
 	
 	var match = /(.*?):relay\(([^)]+)\)$/,
 		combinators = /[+>~\s]/,
@@ -51,22 +51,29 @@ provides: [Element.Delegation]
 			return null;
 		};
 
+	var oldAddEvent = Element.prototype.addEvent,
+		oldRemoveEvent = Element.prototype.removeEvent;
+		
 	Element.implement({
 
 		addEvent: function(type, fn){
 			var splitted = splitType(type);
+			var delegate = splitted.event;
 			if (splitted.selector){
+			splitted.event = (delegate == 'mouseenter') ? 'mouseover' : (delegate == 'mouseleave') ? 'mouseout' : splitted.event;
 				var monitors = this.retrieve('$moo:delegateMonitors', {});
 				if (!monitors[type]){
 					var monitor = function(e){
+						e.delegate = delegate;
+						e.selector = splitted.selector;
 						var el = check.call(this, e, splitted.selector);
 						if (el) this.fireEvent(type, [e, el], 0, el);
 					}.bind(this);
 					monitors[type] = monitor;
-					addEvent.call(this, splitted.event, monitor);
+					oldAddEvent.call(this, splitted.event, monitor);
 				}
 			}
-			return addEvent.apply(this, arguments);
+			return oldAddEvent.apply(this, arguments);
 		},
 
 		removeEvent: function(type, fn){
@@ -75,32 +82,33 @@ provides: [Element.Delegation]
 				var events = this.retrieve('events');
 				if (!events || !events[type] || (fn && !events[type].keys.contains(fn))) return this;
 
-				if (fn) removeEvent.apply(this, [type, fn]);
-				else removeEvent.apply(this, type);
+				if (fn) oldRemoveEvent.apply(this, [type, fn]);
+				else oldRemoveEvent.apply(this, type);
 
 				events = this.retrieve('events');
 				if (events && events[type] && events[type].keys.length == 0){
 					var monitors = this.retrieve('$moo:delegateMonitors', {});
-					removeEvent.apply(this, [splitted.event, monitors[type]]);
+					oldRemoveEvent.apply(this, [splitted.event, monitors[type]]);
 					delete monitors[type];
 				}
 				return this;
 			}
-			return removeEvent.apply(this, arguments);
+
+			return oldRemoveEvent.apply(this, arguments);
 		},
 
 		fireEvent: function(type, args, delay, bind){
 			var events = this.retrieve('events');
 			if (!events || !events[type]) return this;
 			
-			var e = args[0];
-			var el = args[1];
-			var relatedFrom = e.fromElement || e.relatedTarget;
-			var relatedTo = e.toElement || e.relatedTarget;
-			var typeSplit = type.split(':')[0];
-			
-			if(typeSplit == 'mouseover' && el.hasChild(relatedFrom)) return this;
-			if(typeSplit == 'mouseout' && relatedTo && $$(relatedTo.getParents(), relatedTo).contains(el)) return this;  			
+			var e = args[0],
+				el = args[1],
+				relatedFrom = e.fromElement || e.relatedTarget,
+				relatedTo = e.toElement || e.relatedTarget,
+				typeSplit = type.split(':')[0];
+
+			if(e.delegate == 'mouseenter' && el.hasChild(relatedFrom)) return this;
+			if(e.delegate == 'mouseleave' && relatedTo && $$(relatedTo.getParents(), relatedTo).contains(el)) return this;  			
 			
 			events[type].keys.each(function(fn){
 				fn.create({bind: bind || this, delay: delay, arguments: args})();
