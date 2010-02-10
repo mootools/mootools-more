df7ae02d18cf059eadd35ad2fb3226498fb38970
anutron <git@clientcide.com>
2010-02-10 09:52:27 -0800

    welcome to 1.2.4.4

--- Source/Core/More.js
+++ Source/Core/More.js
@@ -21,6 +21,6 @@ provides: [MooTools.More]
 */
 
 MooTools.More = {
-	'version': '1.2.4.3',
-	'build': '6c664838ffa145382c063893d35455a624c13a50'
+	'version': '1.2.4.4',
+	'build': '6f6057dc645fdb7547689183b2311063bd653ddf'
 };
