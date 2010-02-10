5e4c3740926285ee39aef4b032f8a6f69de2135b
Aaron Newton <git@clientcide.com>
2010-02-09 18:29:45 -0800

    welcome to 1.2.4.4dev

--- Source/Core/More.js
+++ Source/Core/More.js
@@ -21,11 +21,6 @@ provides: [MooTools.More]
 */
 
 MooTools.More = {
-<<<<<<< HEAD
 	'version': '1.2.4.4dev',
 	'build': '%build%'
-=======
-	'version': '1.2.4.3',
-	'build': '6c664838ffa145382c063893d35455a624c13a50'
->>>>>>> master
 };
