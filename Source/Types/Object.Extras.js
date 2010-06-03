d89320ed1679fbccf350cf02c3a6af380b68e589
Arian <arian@aryweb.nl>
2010-06-03 17:00:28 +0200

    Rename Native to Types and some Object.Extras adjustments

--- Source/Types/Object.Extras.js
+++ Source/Types/Object.Extras.js
@@ -23,13 +23,11 @@ provides: [Object.Extras, Hash.Extras]
 
 Object.extend({
 
-
-	getFromPath: function(object, notation){
-		var source = object.getClean();
+	getFromPath: function(source, notation){
 		notation.replace(/\[([^\]]+)\]|\.([^.[]+)|[^[.]+/g, function(match){
 			if (!source) return null;
 			var prop = arguments[2] || arguments[1] || arguments[0];
-			source = (prop in source) ? source[prop] : null;
+			source = (prop in source && source.hasOwnProperty(prop)) ? source[prop] : null;
 			return match;
 		});
 		return source;
@@ -40,7 +38,7 @@ Object.extend({
 			return obj != null;
 		};
 		for(key in object){
-			if(!method(object[key])) Object.erase(object,key);
+			if(!method(object[key])) delete object[key];
 		}
 		return object;
 	},
@@ -51,8 +49,9 @@ Object.extend({
 	},
 
 	run: function(object){
+		var args = Array.slice(arguments, 1);
 		for(key in object){
-			if(typeOf(object[key]) == 'function') object[key].run(arguments);
+			if(typeOf(object[key]) == 'function') object[key].apply(object,  args);
 		}
 	}
 
