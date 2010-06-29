b82365b3502334651db984847a2d2c89b9cce0a7
Arian <arian@aryweb.nl>
2010-06-29 11:49:04 +0200

    And the Specs are working now again

---
+++ Specs/Sets.js
@@ -0,0 +1,16 @@
+(function(context){
+	
+context.Sets = {
+
+	'mootools-more': [
+		'Core/Lang', 'Core/Log',
+		'Class/Class.Binds', 'Class/Class.Occlude', 'Class/Class.Refactor',
+		'Element/Element.Forms', 'Element/Element.Measure', 'Element/Elements.From',
+		'Types/Array.Extras', 'Types/Date.Extras', 'Types/Date', 'Types/Hash.Extras',
+		'Types/String.Extras', 'Types/String.QueryString', 'Types/URI', 'Types/URI.Relative',
+		'Utilities/Color', 'Utilities/Hash.Cookie'
+	]
+
+};
+
+})(this.exports || this);
