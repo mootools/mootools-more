b90bbe6fdb88fca85e9da94ac9ef8e56add9030b
Arian <arian@aryweb.nl>
2010-07-03 14:35:34 +0200

    Add Element.Shortcuts Specs

--- Specs/Sets.js
+++ Specs/Sets.js
@@ -5,7 +5,7 @@ context.Sets = {
 	'mootools-more': [
 		'Core/Lang', 'Core/Log',
 		'Class/Class.Binds', 'Class/Class.Occlude', 'Class/Class.Refactor',
-		'Element/Element.Forms', 'Element/Element.Measure', 'Element/Elements.From',
+		'Element/Element.Forms', 'Element/Element.Measure', 'Element/Elements.From', 'Element/Element.Shortcuts',
 		'Types/Array.Extras', 'Types/Date.Extras', 'Types/Date', 'Types/Hash.Extras',
 		'Types/String.Extras', 'Types/String.QueryString', 'Types/URI', 'Types/URI.Relative',
 		'Utilities/Color', 'Utilities/Hash.Cookie'
---
+++ Specs/mootools-more/Element/Element.Shortcuts.js
@@ -0,0 +1,100 @@
+/*
+Script: Element.Shortcuts.js
+	Specs for Element.Shortcuts.js
+
+License:
+	MIT-style license.
+*/
+(function(){
+	
+	var elements;
+	window.addEvent('domready', function(){
+		
+		elements = new Elements([
+			new Element('div', {
+				styles: {
+					display: 'none'
+				}
+			}),
+			new Element('div', {
+				styles: {
+					display: 'block'
+				}
+			}),
+			new Element('div', {
+				styles: {
+					visibility: 'hidden'
+				}
+			}),
+			new Element('div', {
+				styles: {
+					width: 0
+				}
+			}),
+			new Element('div', {
+				'class': 'testClass'
+			})
+		]);
+		
+		elements.inject(new Element('div').inject(document.body));
+		
+	});
+	describe('Element.isDisplayed', {
+
+		'element display should be false': function(){
+			value_of(elements[0].isDisplayed()).should_be_false();
+		},
+
+
+		'element display should be true': function(){
+			value_of(elements[1].isDisplayed()).should_be_true();
+		}
+
+	});
+
+	describe('Element.isVisible', {
+
+		'is the element visible (css visibility)': function(){
+			value_of(elements[2].isVisible()).should_be_false();
+		},
+
+		'is the element visible (width == 0 and height == 0)': function(){
+			value_of(elements[3].isVisible()).should_be_false();
+		}
+
+	});
+
+	describe('Element.toggle', {
+
+		'toggle the display of an element': function(){
+			value_of(elements[1].hide().toggle().isDisplayed()).should_be_true();
+		}
+
+	});
+
+	describe('Element.hide', {
+
+		'hide an element': function(){
+			value_of(elements[1].hide().isDisplayed()).should_be_false();
+		}
+
+	});
+
+	describe('Element.show', {
+
+		'show the element': function(){
+			value_of(elements[0].show().isDisplayed()).should_be_true();
+		}
+
+	});
+
+	describe('Element.swapClass', {
+
+		'should add and remove a clas to the class attribute': function(){
+			var el = elements[4].swapClass('testClass', 'newClass');
+			value_of(el.hasClass('newClass') && !el.hasClass('testClass')).should_be_true();
+		}
+
+	});
+
+})();
