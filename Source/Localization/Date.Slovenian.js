eac807b655e004b8ee0444d468c64478bde4c39c
Radovan Lozej <radovan.lozej@gmail.com>
2010-07-10 21:37:15 +0200

    fixed typo and added some missing spaces

--- Source/Localization/Date.Slovenian.js
+++ Source/Localization/Date.Slovenian.js
@@ -38,53 +38,53 @@ provides: [Date.Slovenian]
 
 		lessThanMinuteAgo: 'manj kot minuto nazaj',
 		minuteAgo: 'minuto nazaj',
-		minutesAgo: function(delta){
-			return '{delta} ' + pluralize(delta, 'minuto', 'minuti', 'minute','minut') + ' nazaj';
+		minutesAgo: function (delta){
+			return '{delta} ' + pluralize(delta, 'minuto', 'minuti', 'minute', 'minut') + ' nazaj';
 		},
 		hourAgo: 'uro nazaj',
-		hoursAgo: function(delta){
-			return '{delta} ' + pluralize(delta, 'uro', 'uri', 'ure','ur') + ' nazaj';
+		hoursAgo: function (delta){
+			return '{delta} ' + pluralize(delta, 'uro', 'uri', 'ure', 'ur') + ' nazaj';
 		},
 		dayAgo: 'dan nazaj',
-		daysAgo: function(delta){
-			return '{delta} ' + pluralize(delta, 'dan', 'dneva', 'dni','dni') + ' nazaj';
+		daysAgo: function (delta){
+			return '{delta} ' + pluralize(delta, 'dan', 'dneva', 'dni', 'dni') + ' nazaj';
 		},
 		weekAgo: 'teden nazaj',
-		weeksAgo: function(delta){
-			return '{delta} ' + pluralize(delta, 'teden', 'tedna', 'tedne','tednov') + ' nazaj';
+		weeksAgo: function (delta){
+			return '{delta} ' + pluralize(delta, 'teden', 'tedna', 'tedne', 'tednov') + ' nazaj';
 		},
 		monthAgo: 'mesec nazaj',
-		monthsAgo: function(delta){
-			return '{delta} ' + pluralize(delta, 'mesec', 'meseca', 'mesece','mesecov') + ' nazaj';
+		monthsAgo: function (delta){
+			return '{delta} ' + pluralize(delta, 'mesec', 'meseca', 'mesece', 'mesecov') + ' nazaj';
 		},
 		yearthAgo: 'leto nazaj',
-		yearsAgo: function(delta){
-			return '{delta} ' + pluralize(delta, 'leto', 'leti', 'leta','let') + ' nazaj';
+		yearsAgo: function (delta){
+			return '{delta} ' + pluralize(delta, 'leto', 'leti', 'leta', 'let') + ' nazaj';
 		},
 		lessThanMinuteUntil: 'še manj kot minuto',
 		minuteUntil: 'še minuta',
-		minutesUntil: function(delta){
-			return 'še {delta} ' + pluralize(delta, 'minuta', 'minuti', 'minute','minut');
+		minutesUntil: function (delta){
+			return 'še {delta} ' + pluralize(delta, 'minuta', 'minuti', 'minute', 'minut');
 		},
 		hourUntil: 'še ura',
-		hoursUntil: function(delta){
-			return 'še {delta} ' + pluralize(delta, 'ura', 'uri', 'ure','ur');
+		hoursUntil: function (delta){
+			return 'še {delta} ' + pluralize(delta, 'ura', 'uri', 'ure', 'ur');
 		},
 		dayUntil: 'še dan',
-		daysUntil: function(delta){
-			return 'še {delta} ' + pluralize(delta, 'dan', 'dneva', 'dnevi','dni');
+		daysUntil: function (delta){
+			return 'še {delta} ' + pluralize(delta, 'dan', 'dneva', 'dnevi', 'dni');
 		},
 		weekUntil: 'še tedn',
-		weeksUntil: function(delta){
-			return 'še {delta} ' + pluralize(delta, 'teden', 'tedna', 'tedni','tednov');
+		weeksUntil: function (delta){
+			return 'še {delta} ' + pluralize(delta, 'teden', 'tedna', 'tedni', 'tednov');
 		},
 		monthUntil: 'še mesec',
-		monthsUntil: function(delta){
-			return 'še {delta} ' + pluralize(delta, 'mesec', 'meseca', 'meseci','mesecov');
+		monthsUntil: function (delta){
+			return 'še {delta} ' + pluralize(delta, 'mesec', 'meseca', 'meseci', 'mesecov');
 		},
 		yearUntil: 'še leto',
-		yearsUntil: function(delta){
-			return 'še {delta} ' + pluralize(delta, 'leto', 'leti', 'leta','tlet');
+		yearsUntil: function (delta){
+			return 'še {delta} ' + pluralize(delta, 'leto', 'leti', 'leta', 'let');
 		}
 
 	});
