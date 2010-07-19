fc209b72fb30e60913dccce399ec4dcd41a265b6
Arian <arian@aryweb.nl>
2010-07-19 20:15:22 +0200

    Add Events.Extras Specs

---
+++ Specs/mootools-more/Class/Events.Extras.js
@@ -0,0 +1,55 @@
+/*
+Script: Events.Extras.js
+	Specs for Events.Extras.js
+
+License:
+	MIT-style license.
+*/
+(function(){
+	
+	
+	
+	describe('Events.Extras', {
+
+		'tests the :once pseudo': function(){
+			var e = new Events();
+			var counter = 0;
+			e.addEvent('connect:once', function(){
+				counter++;
+			});
+			e.fireEvent('connect');
+			e.fireEvent('connect');			
+			e.fireEvent('connect');
+			
+			value_of(counter).should_be(1);
+		},
+		
+		'tests the Events.definePseudo function': function(){
+			
+			var eventFn =  function(){
+				return 'bar';
+			},
+			eventArgs = ['one', 'two', 'three'];
+			
+			Events.definePseudo('test', function(split, fn, args){
+				value_of(split).should_be({
+					event: 'e',
+					selector: 'foo',
+					pseudo: 'test',
+					original: 'e:test(foo)'
+				});
+				value_of(fn).should_be(eventFn);
+				value_of(Array.from(args)).should_be(eventArgs);
+			});
+			
+			var e = new Events();
+			e.addEvent('e:test(foo)', eventFn);
+			e.fireEvent('e', eventArgs);
+		}
+
+
+	});
+
+})();
+
+
