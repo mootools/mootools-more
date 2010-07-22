7eb252f3136b93a723598f39d3b3a2a3a35eb2d1
Arian <arian@aryweb.nl>
2010-07-22 19:59:50 +0200

    Update Specs for Events.Pseudos

--- Specs/Sets.js
+++ Specs/Sets.js
@@ -4,7 +4,7 @@ context.Sets = {
 
 	'mootools-more': [
 		'Core/Lang', 'Core/Log',
-		'Class/Class.Binds', 'Class/Class.Occlude', 'Class/Class.Refactor', 'Class/Events.Extras',
+		'Class/Class.Binds', 'Class/Class.Occlude', 'Class/Class.Refactor', 'Class/Events.Pseudos',
 		'Element/Element.Forms', 'Element/Element.Measure', 'Element/Elements.From', 'Element/Element.Shortcuts',
 		'Types/Array.Extras', 'Types/Date.Extras', 'Types/Date', 'Types/Object.Extras', 'Types/Hash.Extras',
 		'Types/String.Extras', 'Types/String.QueryString', 'Types/URI', 'Types/URI.Relative',
