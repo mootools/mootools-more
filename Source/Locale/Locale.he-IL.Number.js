937575a1931e69a4ec812e929a853a104d21dc5f
Tim Wienk <tim@wienk.name>
2010-08-29 00:31:40 +0200

    Adding Elad Ossadon's Hebrew translations.

---
+++ Source/Locale/Locale.he-IL.Number.js
@@ -0,0 +1,51 @@
+/*
+---
+
+name: Locale.he-IL.Number
+
+description: Number messages for Hebrew.
+
+license: MIT-style license
+
+authors:
+  - Elad Ossadon
+
+requires:
+  - /Locale
+
+provides: [Locale.he-IL.Number]
+
+...
+*/
+
+Locale.define('he-IL', 'Number', {
+
+	decimal: '.',
+	group: ',',
+/*
+	decimals: 0,
+	precision: 0,
+	scientific: null,
+
+	prefix: null,
+	suffic: null,
+*/
+	// Negative/Currency/percentage will mixin Number
+	negative: {
+		prefix: '-'
+	},
+
+	currency: {
+		decimals: 2,
+		suffix: ' ₪',
+		'scientific': false
+	},
+
+	percentage: {
+		decimals: 2,
+		suffix: '%'
+	}
+
+});
+
+
