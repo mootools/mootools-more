870c7fe4e65d25339449011219a77aa2368f28db
Tim Wienk <tim@wienk.name>
2010-08-29 00:34:40 +0200

    Excess whitespace.

--- Source/Locale/Locale.EU.Number.js
+++ Source/Locale/Locale.EU.Number.js
@@ -19,26 +19,26 @@ provides: [Locale.EU.Number]
 */
 
 Locale.define('EU', 'Number', {
-	
+
 	decimal: ',',
 	group: '.',
-/*	
+/*
 	decimals: 0,
 	precision: 0,
-*/	
+*/
 	// Negative/Currency/percentage will mixin Number
 	negative: {
 		prefix: '-'
 	},
-	
+
 	currency: {
 		decimals: 2,
-		prefix: '€ '	
+		prefix: '€ '
 	},
-	
+
 	percentage: {
 		decimals: 2,
-		suffix: '%'		
+		suffix: '%'
 	}
 
 });
