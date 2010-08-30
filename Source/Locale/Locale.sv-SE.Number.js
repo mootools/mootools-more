e2cfe37ea0d022285ab5f29c48d7f5ea52e24e86
Arian <arian@aryweb.nl>
2010-08-30 11:02:42 +0200

    Add Locale.sv-SE.Number

---
+++ Source/Locale/Locale.sv-SE.Number.js
@@ -0,0 +1,29 @@
+/* 
+---
+
+name: Locale.nl-NL.Number
+
+description: Number messages for Swedish.
+
+license: MIT-style license
+
+authors:
+  - Arian Stolwijk
+  - Martin Lundgren
+
+requires:
+  - /Locale
+  - /Locale.EU.Number
+
+provides: [Locale.sv-SE.Number]
+
+...
+*/
+
+Locale.define('sv-SE', 'Number', { 
+
+	currency: { 
+		prefix: 'SEK ' 
+	}
+
+}).inherit('EU', 'Number');
