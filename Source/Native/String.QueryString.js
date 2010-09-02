9380198355b6d2318a4fb6a8f5f92a725eb41f8c
Christopher Pitt <chris@sixtyseconds.co.za>
2010-09-02 09:36:10 +0200

    Changed the docs/argument names to more sensibly process keys/values.

--- Source/Native/String.QueryString.js
+++ Source/Native/String.QueryString.js
@@ -11,8 +11,8 @@ authors:
 - Sebastian Markbåge, Aaron Newton, Lennart Pilon, Valerio Proietti
 
 requires:
-- core:1.2.4/Array
-- core:1.2.4/String
+- Core:1.2.4/Array
+- Core:1.2.4/String
 - /MooTools.More
 
 provides: [String.QueryString]
@@ -22,14 +22,17 @@ provides: [String.QueryString]
 
 String.implement({
 
-	parseQueryString: function(){
+	parseQueryString: function(decodeKeys, decodeValues){
+		var decodeKeys = typeof decodeKeys == 'undefined' ? true : decodeKeys;
+		var decodeValues = typeof decodeValues == 'undefined' ? true : decodeValues;
 		var vars = this.split(/[&;]/), res = {};
 		if (vars.length) vars.each(function(val){
 			var index = val.indexOf('='),
 				keys = index < 0 ? [''] : val.substr(0, index).match(/[^\]\[]+/g),
-				value = decodeURIComponent(val.substr(index + 1)),
+				value = decodeValues ? decodeURIComponent(val.substr(index + 1)) : val.substr(index + 1),
 				obj = res;
 			keys.each(function(key, i){
+				var key = decodeKeys ? decodeURIComponent(key) : key;
 				var current = obj[key];
 				if(i < keys.length - 1)
 					obj = obj[key] = current || {};
