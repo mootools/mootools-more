3fd797e0f9ec762750399ac2776f355b439f7fc3
Christopher Pitt <chris@sixtyseconds.co.za>
2010-09-02 09:36:10 +0200

    Lighthouse-285. URI.setData encoding is not consistent

--- Source/Native/String.QueryString.js
+++ Source/Native/String.QueryString.js
@@ -24,14 +24,17 @@ provides: [String.QueryString]
 
 String.implement({
 
-	parseQueryString: function(){
+	parseQueryString: function(decodeKeys, decodeValues){
+		if (decodeKeys == null) decodeKeys = true;
+		if (decodeValues == null) decodeValues = true;
 		var vars = this.split(/[&;]/), res = {};
 		if (vars.length) vars.each(function(val){
 			var index = val.indexOf('='),
-				keys = index < 0 ? [''] : val.substr(0, index).match(/[^\]\[]+/g),
-				value = decodeURIComponent(val.substr(index + 1)),
+				keys = index < 0 ? [''] : val.substr(0, index).match(/([^\]\[]+|(\B)(?=\]))/g),
+				value = decodeValues ? decodeURIComponent(val.substr(index + 1)) : val.substr(index + 1),
 				obj = res;
 			keys.each(function(key, i){
+				if (decodeKeys) key = decodeURIComponent(key);
 				var current = obj[key];
 				if(i < keys.length - 1)
 					obj = obj[key] = current || {};
