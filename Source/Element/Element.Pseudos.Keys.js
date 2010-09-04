22a30c94e3b346b16eafab15de2cc53ed4ad2b42
Arian <arian@aryweb.nl>
2010-09-04 13:32:36 +0200

    * Support for keyup Element.Pseudos.Keys (for example keyup:keys(shift+b) ) * Move the keycodes extension to Element.Pseudo.Keys (Keyboard requires Element.Pseudo.Keys)

--- Source/Element/Element.Pseudos.Keys.js
+++ Source/Element/Element.Pseudos.Keys.js
@@ -1,7 +1,7 @@
 /*
 ---
 
-name: Element.Pseudos
+name: Element.Pseudos.Keys
 
 description: Adds functionallity fire events if certain keycombinations are pressed
 
@@ -24,28 +24,50 @@ var keysStoreKey = '$moo:keys-pressed',
 
 Event.definePseudo('keys', function(split, fn, args){
 
-	if (split.event != 'keydown') return;
-	
 	var event = args[0],
 		keys = split.value.split('+'),
 		pressed = this.retrieve(keysStoreKey, []);
-	
+
 	pressed.include(event.key);
-	
+
 	if (keys.every(function(key){
 		return pressed.contains(key);
 	})) fn.apply(this, args);
 	
 	this.store(keysStoreKey, pressed);
-	
-	
+
 	if (!this.retrieve(keysKeyupStoreKey)){
-		var keyup = function(){
-			this.store(keysStoreKey, []);
+		var keyup = function(event){
+			pressed = this.retrieve(keysStoreKey).erase(event.key);
+			this.store(keysStoreKey, pressed);
 		};
 		this.store(keysKeyupStoreKey, keyup).addEvent('keyup', keyup);
 	}
-		
+
+});
+
+Object.append(Event.Keys, {
+	'shift': 16,
+	'control': 17,
+	'alt': 18,
+	'capslock': 20,
+	'pageup': 33,
+	'pagedown': 34,
+	'end': 35,
+	'home': 36,
+	'numlock': 144,
+	'scrolllock': 145,
+	';': 186,
+	'=': 187,
+	',': 188,
+	'-': Browser.firefox ? 109 : 189,
+	'.': 190,
+	'/': 191,
+	'`': 192,
+	'[': 219,
+	'\\': 220,
+	']': 221,
+	"'": 222
 });
 
 })();
