29e13b3be79802411577a0c6cdad22853c55d699
Jacob Thornton <jacobthornton@gmail.com>
2010-09-30 21:53:16 -0700

    lighthouse - 395, Element.Pin jumps on iniailize
    https://mootools.lighthouseapp.com/projects/24057-mootoolsmore/tickets/395-elementpin-jumps-on-initialize#ticket-395-3
    
    - the problem was that the pinned element was getting it's position from
      it's offest parent -- but if that offset element had a position
      of relative its percieved position would be different that of the body
      (which is really what it should be relative to when setting to position fixed.)
      that was a terrible explanation, i hope that
      made a little sense...

--- Source/Element/Element.Pin.js
+++ Source/Element/Element.Pin.js
@@ -45,7 +45,7 @@ provides: [Element.Pin]
 				scroll = window.getScroll();
 
 			if (enable !== false){
-				pinnedPosition = this.getPosition(this.getOffsetParent());
+				pinnedPosition = this.getPosition(document.body);
 				if (!this.retrieve('pin:_pinned')){
 					var currentPosition = {
 						top: pinnedPosition.y - scroll.y,
