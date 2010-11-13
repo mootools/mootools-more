52956267002d845090daf71ca5f4fbfefa1cb26f
Arian <arian@aryweb.nl>
2010-11-14 00:57:08 +0100

    Spaces -> Tabs + some other minor style tweaks

--- Source/Drag/Sortables.Delegation.js
+++ Source/Drag/Sortables.Delegation.js
@@ -13,9 +13,9 @@ authors:
   - Jacob Thornton
 
 requires:
-  - /Element.Delegation
-  - /Drag.Move
-  - /Sortables
+  - Element.Delegation
+  - Drag.Move
+  - Sortables
 
 provides: [Sortables.Delegation]
 
@@ -24,41 +24,41 @@ provides: [Sortables.Delegation]
 
 Sortables.Delegation = new Class({
 
-  Extends: Sortables,
+	Extends: Sortables,
 
 	options: {
-    relay: '*'
-  },
-
-	initialize: function(lists, options){
-    this.parent(lists, options);
+		relay: '*'
 	},
 
-  addLists: function(){
-    Array.flatten(arguments).each(function(list){
-      this.lists.push(list);
-      this.relay = this.relay || this.options.relay + (this.options.handle ? ' ' + this.options.handle : '');
-      var start = list.retrieve('sortables:start', this.start.bind(this));
-      list.addEvent('mousedown:relay(' + this.relay + ')', start);
-    }, this);
-    return this;
-  },
+	initialize: function(lists, options){
+		this.parent(lists, options);
+	},
 
-  start: function(event, element){
-    var match;
-    while (!match) {
-      match = this.lists.contains(element.getParent());
-      if (!match) element = element.getParent(this.relay);
-    }
-    this.parent(event, element);
-  },
+	addLists: function(){
+		Array.flatten(arguments).each(function(list){
+			this.lists.push(list);
+			if (!this.relay) this.relay = this.options.relay + (this.options.handle ? ' ' + this.options.handle : '');
+			var start = list.retrieve('sortables:start', this.start.bind(this));
+			list.addEvent('mousedown:relay(' + this.relay + ')', start);
+		}, this);
+		return this;
+	},
 
-  removeLists: function(){
-    return $$(Array.flatten(arguments).map(function(list){
-      this.lists.erase(list);
-      list.removeEvent('mousedown:relay(' + this.relay + ')', list.retrieve('sortables:start')); 
-      return list;
-    }, this));
-  }
+	start: function(event, element){
+		var match;
+		while (!match){
+			match = this.lists.contains(element.getParent());
+			if (!match) element = element.getParent(this.relay);
+		}
+		this.parent(event, element);
+	},
+
+	removeLists: function(){
+		return $$(Array.flatten(arguments).map(function(list){
+			this.lists.erase(list);
+			list.removeEvent('mousedown:relay(' + this.relay + ')', list.retrieve('sortables:start'));
+			return list;
+		}, this));
+	}
 
 });
