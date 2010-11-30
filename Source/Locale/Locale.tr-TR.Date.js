bd296491e43630a45a8dd52078036b21bb86e430
farkob <farkob@gmail.com>
2010-11-30 18:48:11 +0200

    Turkish locale and encoding fix

--- Source/Locale/Locale.tr-TR.Date.js
+++ Source/Locale/Locale.tr-TR.Date.js
@@ -20,10 +20,10 @@ provides: [Locale.tr-TR.Date]
 
 Locale.define('tr-TR', 'Date', {
 
-	months: ['Ocak', 'Þubat', 'Mart', 'Nisan', 'Mayýs', 'Haziran', 'Temmuz', 'Aðustos', 'Eylül', 'Ekim', 'Kasým', 'Aralýk'],
-	months_abbr: ['Oca', 'Þub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'Aðu', 'Eyl', 'Eki', 'Kas', 'Ara'],
-	days: ['Pazar', 'Pazartesi', 'Salý', 'Çarþamba', 'Perþembe', 'Cuma', 'Cumartesi'],
-	days_abbr: ['Pa', 'Pzt', 'Sa', 'Ça', 'Pe', 'Cu', 'Cmt'],
+	months: ['Ocak', 'Åžubat', 'Mart', 'Nisan', 'MayÄ±s', 'Haziran', 'Temmuz', 'AÄŸustos', 'EylÃ¼l', 'Ekim', 'KasÄ±m', 'AralÄ±k'],
+	months_abbr: ['Oca', 'Åžub', 'Mar', 'Nis', 'May', 'Haz', 'Tem', 'AÄŸu', 'Eyl', 'Eki', 'Kas', 'Ara'],
+	days: ['Pazar', 'Pazartesi', 'SalÄ±', 'Ã‡arÅŸamba', 'PerÅŸembe', 'Cuma', 'Cumartesi'],
+	days_abbr: ['Pa', 'Pzt', 'Sa', 'Ã‡a', 'Pe', 'Cu', 'Cmt'],
 
 	// Culture's date order: MM/DD/YYYY
 	dateOrder: ['date', 'month', 'year'],
@@ -35,32 +35,32 @@ Locale.define('tr-TR', 'Date', {
 	// Date.Extras
 	ordinal: '',
 
-	lessThanMinuteAgo: 'bir dakikadan önce',
-	minuteAgo: 'yaklaþýk bir dakika önce',
-	minutesAgo: '{delta} dakika önce',
-	hourAgo: 'bir saat kadar önce',
-	hoursAgo: '{delta} saat kadar önce',
-	dayAgo: 'bir gün önce',
-	daysAgo: '{delta} gün önce',
-	weekAgo: 'bir hafta önce',
-	weeksAgo: '{delta} hafta önce',
-	monthAgo: 'bir ay önce',
-	monthsAgo: '{delta} ay önce',
-	yearAgo: 'bir yýl önce',
-	yearsAgo: '{delta} yýl önce',
+	lessThanMinuteAgo: 'bir dakikadan Ã¶nce',
+	minuteAgo: 'yaklaÅŸÄ±k bir dakika Ã¶nce',
+	minutesAgo: '{delta} dakika Ã¶nce',
+	hourAgo: 'bir saat kadar Ã¶nce',
+	hoursAgo: '{delta} saat kadar Ã¶nce',
+	dayAgo: 'bir gÃ¼n Ã¶nce',
+	daysAgo: '{delta} gÃ¼n Ã¶nce',
+	weekAgo: 'bir hafta Ã¶nce',
+	weeksAgo: '{delta} hafta Ã¶nce',
+	monthAgo: 'bir ay Ã¶nce',
+	monthsAgo: '{delta} ay Ã¶nce',
+	yearAgo: 'bir yÄ±l Ã¶nce',
+	yearsAgo: '{delta} yÄ±l Ã¶nce',
 
 	lessThanMinuteUntil: 'bir dakikadan az sonra',
 	minuteUntil: 'bir dakika kadar sonra',
 	minutesUntil: '{delta} dakika sonra',
 	hourUntil: 'bir saat kadar sonra',
 	hoursUntil: '{delta} saat kadar sonra',
-	dayUntil: 'bir gün sonra',
-	daysUntil: '{delta} gün sonra',
+	dayUntil: 'bir gÃ¼n sonra',
+	daysUntil: '{delta} gÃ¼n sonra',
 	weekUntil: 'bir hafta sonra',
 	weeksUntil: '{delta} hafta sonra',
 	monthUntil: 'bir ay sonra',
 	monthsUntil: '{delta} ay sonra',
-	yearUntil: 'bir yýl sonra',
-	yearsUntil: '{delta} yýl sonra'
+	yearUntil: 'bir yÄ±l sonra',
+	yearsUntil: '{delta} yÄ±l sonra'
 
 });
