b77bd625365e623158142a02786b4d6bf7f4532f
Arian <arian@aryweb.nl>
2011-01-12 17:20:21 +0100

    Fix Fx.Move - The instance options were modified by the options passed into the start method by Object.merge

--- Source/Fx/Fx.Move.js
+++ Source/Fx/Fx.Move.js
@@ -38,7 +38,7 @@ Fx.Move = new Class({
 		if (topLeft.top == 'auto' || topLeft.left == 'auto'){
 			element.setPosition(element.getPosition(element.getOffsetParent()));
 		}
-		return this.parent(element.position(Object.merge(this.options, destination, {returnPos: true})));
+		return this.parent(element.position(Object.merge({}, this.options, destination, {returnPos: true})));
 	}
 
 });
