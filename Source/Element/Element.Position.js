7405b73ba3f9165ffd4daa80959b3598a10e56e9
Arian <arian@aryweb.nl>
2011-01-21 23:38:15 +0100

    * Continued improving Element.Position
    - used a local variable wich contains the object with methods, faster than accessing `this` (Element.Position is still accessible)
    - position object is pased 'by reference' so no need to return the position variable each time
    - toMinimumMaximum -> toMinMax
    - removed place, now only getPosition
    - do the option checks (`if (options.edge)`) in the getPosition method before calling the methods
    - added a Element.calculatePosition (any better methodname?) if you need the position object, position will only position the element (with backward compat for the returnPos option)

--- Source/Element/Element.Position.js
+++ Source/Element/Element.Position.js
@@ -16,26 +16,27 @@ authors:
 requires:
   - Core/Options
   - Core/Element.Dimensions
-  - /Element.Measure
+  - Element.Measure
 
 provides: [Element.Position]
 
 ...
 */
 
-(function(){
+(function(original){
 
-Element.Position = {
+// Using a local variable seems to be faster than using `this`: http://jsperf.com/this-vs-var
+var local = Element.Position = {
 
-  options: {/*
+	options: {/*
 		edge: false,
 		returnPos: false,
-		minimum: { x: 0, y: 0 },
-		maximum: { x: 0, y: 0},
+		minimum: {x: 0, y: 0},
+		maximum: {x: 0, y: 0},
 		relFixedPosition: false,
 		ignoreMargins: false,
 		ignoreScroll: false,
-		allowNegative: false*/
+		allowNegative: false,*/
 		relativeTo: document.body,
 		position: {
 			x: 'center', //left, center, right
@@ -47,31 +48,24 @@ Element.Position = {
 		}
 	},
 
-	place: function(element, options){
-		var options = this.getOptions(element, options),
-			position = this.getPosition(element, options);
-
-		if (options.returnPos) return position;
-		return element.setStyles(position);
-	},
-
 	getOptions: function(element, options){
-		var options = Object.merge({}, this.options, options);
-		options.position = this.getPositionOption(options);
-		options.edge = this.getEdgeOption(options);
-		options.offset = this.getOffsetOption(element, options);
-		options.dimensions = this.getDimensionsOption(element, options);
+		var options = Object.merge({}, local.options, options);
+		options.position = local.getPositionOption(options);
+		options.edge = local.getEdgeOption(options);
+		options.offset = local.getOffsetOption(element, options);
+		options.dimensions = local.getDimensionsOption(element, options);
 		return options;
 	},
 
 	getPositionOption: function(options){
-		return this.getCoordinateFromValue(options.position);
+		return local.getCoordinateFromValue(options.position);
 	},
 
 	getEdgeOption: function(options){
-		var edgeOption = this.getCoordinateFromValue(options.edge);
-		if (edgeOption) return edgeOption;
-		return options.position.x == 'center' && options.position.y == 'center' ? {x: 'center', y: 'center'} : {x: 'left', y: 'top'};
+		var edgeOption = local.getCoordinateFromValue(options.edge);
+		return edgeOption ? edgeOption :
+			(options.position.x == 'center' && options.position.y == 'center') ? {x: 'center', y: 'center'} :
+			{x: 'left', y: 'top'};
 	},
 
 	getOffsetOption: function(element, options){
@@ -100,174 +94,141 @@ Element.Position = {
 		}
 	},
 
-	getDimensionsOption: function(element, options){
+	getDimensionsOption: function(element){
 		return element.getDimensions({
 			computeSize: true,
-			styles:['padding', 'border', 'margin']
+			styles: ['padding', 'border', 'margin']
 		});
 	},
 
 	getPosition: function(element, options){
-		var position = {},
+		var position = {position: 'absolute'},
+			options = local.getOptions(element, options),
 			relativeTo = document.id(options.relativeTo) || document.body,
-			calc = relativeTo == document.body ? window.getScroll() : relativeTo.getPosition(),
+			calc = (relativeTo == document.body) ? window.getScroll() : relativeTo.getPosition(),
 			top = calc.y,
 			left = calc.x,
 			offsetY = options.offset.y,
 			offsetX = options.offset.x,
 			winSize = window.getSize();
 
+		// TODO: options.position in separate function?
 		switch(options.position.x){
-			case 'left':
-				position.x = left + offsetX;
-				break;
-			case 'right':
-				position.x = left + offsetX + relativeTo.offsetWidth;
-				break;
-			default: //center
-				position.x = left + ((relativeTo == document.body ? winSize.x : relativeTo.offsetWidth) / 2) + offsetX;
-				break;
+			case 'left': position.x = left + offsetX; break;
+			case 'right': position.x = left + offsetX + relativeTo.offsetWidth; break;
+			// center
+			default: position.x = left + ((relativeTo == document.body ? winSize.x : relativeTo.offsetWidth) / 2) + offsetX; break;
 		}
 
 		switch(options.position.y){
-			case 'top':
-				position.y = top + offsetY;
-				break;
-			case 'bottom':
-				position.y = top + offsetY + relativeTo.offsetHeight;
-				break;
-			default: //center
-				position.y = top + ((relativeTo == document.body ? winSize.y : relativeTo.offsetHeight) / 2) + offsetY;
-				break;
+			case 'top': position.y = top + offsetY; break;
+			case 'bottom': position.y = top + offsetY + relativeTo.offsetHeight; break;
+			// center
+			default: position.y = top + ((relativeTo == document.body ? winSize.y : relativeTo.offsetHeight) / 2) + offsetY; break;
 		}
 
-		position = this.toEdge(position, options);
+		if (options.edge) local.toEdge(position, options);
 
-		position = {
-			position: 'absolute',
-			left: ((position.x >= 0 || options.offset.parentPositioned || options.allowNegative) ? position.x : 0).toInt(),
-			top: ((position.y >= 0 || options.offset.parentPositioned || options.allowNegative) ? position.y : 0).toInt()
-		};
+		var offset = options.offset;
+		position.left = ((position.x >= 0 || offset.parentPositioned || options.allowNegative) ? position.x : 0).toInt();
+		position.top = ((position.y >= 0 || offset.parentPositioned || options.allowNegative) ? position.y : 0).toInt();
 
-		position = this.toMinimumMaximum(position, options);
-		position = this.toRelFixedPosition(relativeTo, position, options);
-		position = this.toIgnoreScroll(relativeTo, position, options);
-		position = this.toIgnoreMargins(position, options);
+		local.toMinMax(position, options);
+
+		if (options.relFixedPosition || relativeTo.getStyle('position') == 'fixed') local.toRelFixedPosition(relativeTo, position);
+		if (options.ignoreScroll) local.toIgnoreScroll(relativeTo, position);
+		if (options.ignoreMargins) local.toIgnoreMargins(position, options);
 
 		position.left = Math.ceil(position.left);
 		position.top = Math.ceil(position.top);
+
 		return position;
 	},
 
-	toMinimumMaximum: function(position, options){
-		var xy = {left: 'x', top: 'y'};
+	toMinMax: function(position, options){
+		var xy = {left: 'x', top: 'y'}, value;
 		['minimum', 'maximum'].each(function(minmax){
 			['left', 'top'].each(function(lr){
-				var value = options[minmax] ? options[minmax][xy[lr]] : null;
+				value = options[minmax] ? options[minmax][xy[lr]] : null;
 				if (value != null && ((minmax == 'minimum') ? position[lr] < value : position[lr] > value)) position[lr] = value;
-			}, this);
-		}, this);
-		return position;
+			});
+		});
 	},
 
-	toRelFixedPosition: function(relativeTo, position, options){
-		if (relativeTo.getStyle('position') != 'fixed' && !options.relFixedPosition) return position;
+	toRelFixedPosition: function(relativeTo, position){
 		var winScroll = window.getScroll();
 		position.top += winScroll.y;
 		position.left += winScroll.x;
-		return position;
 	},
 
-	toIgnoreScroll: function(relativeTo, position, options){
-		if (!options.ignoreScroll) return position;
+	toIgnoreScroll: function(relativeTo, position){
 		var relScroll = relativeTo.getScroll();
 		position.top -= relScroll.y;
 		position.left -= relScroll.x;
-		return position;
 	},
 
 	toIgnoreMargins: function(position, options){
-		if (!options.ignoreMargins) return position;
-		position.left += (
-			options.edge.x == 'right' ? options.dimensions['margin-right'] :
-			options.edge.x == 'center' ? -options.dimensions['margin-left'] + ((options.dimensions['margin-right'] + options.dimensions['margin-left'])/2) :
-				- options.dimensions['margin-left']
-		);
-		position.top += (
-			options.edge.y == 'bottom' ? options.dimensions['margin-bottom'] :
-			options.edge.y == 'center' ? -options.dimensions['margin-top'] + ((options.dimensions['margin-bottom'] + options.dimensions['margin-top'])/2) :
-				- options.dimensions['margin-top']
-		);
-		return position;
+		position.left += options.edge.x == 'right' ? options.dimensions['margin-right'] :
+			options.edge.x != 'center' ? -options.dimensions['margin-left'] :
+			-options.dimensions['margin-left'] + ((options.dimensions['margin-right'] + options.dimensions['margin-left']) / 2);
+
+		position.top += options.edge.y == 'bottom' ? options.dimensions['margin-bottom'] :
+			options.edge.y != 'center' ? -options.dimensions['margin-top'] :
+			-options.dimensions['margin-top'] + ((options.dimensions['margin-bottom'] + options.dimensions['margin-top']) / 2);
 	},
 
 	toEdge: function(position, options){
-		if (!options.edge) return position;
+		var edgeOffset = {},
+			dimensions = options.dimensions,
+			edge = options.edge;
 
-		var edgeOffset = {};
-
-		switch(options.edge.x){
-			case 'left':
-				edgeOffset.x = 0;
-				break;
-			case 'right':
-				edgeOffset.x = -options.dimensions.x - options.dimensions.computedRight - options.dimensions.computedLeft;
-				break;
-			default: //center
-				edgeOffset.x = -(options.dimensions.totalWidth/2);
-				break;
+		switch(edge.x){
+			case 'left': edgeOffset.x = 0; break;
+			case 'right': edgeOffset.x = -dimensions.x - dimensions.computedRight - dimensions.computedLeft; break;
+			// center
+			default: edgeOffset.x = -(dimensions.totalWidth / 2); break;
 		}
 
-		switch(options.edge.y){
-			case 'top':
-				edgeOffset.y = 0;
-				break;
-			case 'bottom':
-				edgeOffset.y = -options.dimensions.y - options.dimensions.computedTop - options.dimensions.computedBottom;
-				break;
-			default: //center
-				edgeOffset.y = -(options.dimensions.totalHeight/2);
-				break;
+		switch(edge.y){
+			case 'top': edgeOffset.y = 0; break;
+			case 'bottom': edgeOffset.y = -dimensions.y - dimensions.computedTop - dimensions.computedBottom; break;
+			// center
+			default: edgeOffset.y = -(dimensions.totalHeight / 2); break;
 		}
 
 		position.x += edgeOffset.x;
 		position.y += edgeOffset.y;
-		return position;
 	},
 
 	getCoordinateFromValue: function(option){
 		if (typeOf(option) != 'string') return option;
-		]option = option.toLowerCase();
+		option = option.toLowerCase();
 
-		var value = {};
+		var value = {x: 'center', y: 'center'};
 
-		if (option.test('left')){
-			value.x = 'left';
-		} else if (option.test('right')){
-			value.x = 'right';
-		} else {
-			value.x = 'center';
-		}
+		if (option.test('left')) value.x = 'left';
+		else if (option.test('right')) value.x = 'right';
 
-		if (option.test('upper') || option.test('top')){
-			value.y = 'top';
-		} else if (option.test('bottom')){
-			value.y = 'bottom';
-		} else {
-			value.y = 'center';
-		}
+		if (option.test(/upper|top/)) value.y = 'top';
+		else if (option.test('bottom')) value.y = 'bottom';
+
 		return value;
 	}
 
 };
 
-var original = Element.prototype.position;
-
 Element.implement({
+
 	position: function(options){
-		if (typeOf(options) == 'array') return position.original ? position.original.apply(this.element, arguments) : this;
-		return Element.Position.place(this, options);
+		if (typeOf(options) == 'array') return (original ? original.apply(this, arguments) : this);
+		var position = this.calculatePosition(options);
+		return (options && options.returnPos) ? position : this.setStyles(position);
+	},
+
+	calculatePosition: function(options){
+		return local.getPosition(this, options);
 	}
+
 });
 
-})();
+})(Element.prototype.position);
