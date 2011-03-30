b63c4ab2ffbf48671e7c16a049dcd54cfdf2cfca
Sean McArthur <sean.monstar@gmail.com>
2011-03-29 17:33:49 -0700

    removed modifiers, pressed array will clear when a combination is found

--- Source/Element/Element.Event.Pseudos.Keys.js
+++ Source/Element/Element.Event.Pseudos.Keys.js
@@ -27,8 +27,7 @@ var keysStoreKey = '$moo:keys-pressed',
 	},
 	retrieve = function(key, def){
 		return this.retrieve ? this.retrieve(key, def) : (this[key] || def);
-	},
-	modifiers = ['meta'];
+	};
 
 Event.definePseudo('keys', function(split, fn, args){
 	var event = args[0],
@@ -45,16 +44,15 @@ Event.definePseudo('keys', function(split, fn, args){
 	});
 
 	pressed.include(event.key);
-	
-	modifiers.each(function(mod) {
-		if(event[mod]) pressed.include(mod);
-	});
 
 	if (keyCombos.some(function(combo){
 		return combo.every(function(key){
 			return pressed.contains(key);
 		});
-	})) fn.apply(this, args);
+	})){
+		fn.apply(this, args);
+		pressed = [];
+	}
 
 	store.call(this, keysStoreKey, pressed);
 
@@ -62,9 +60,6 @@ Event.definePseudo('keys', function(split, fn, args){
 		var keyup = function(event){
 			(function(){
 				pressed = retrieve.call(this, keysStoreKey, []).erase(event.key);
-				modifiers.each(function(mod) {
-					if(!event[mod]) pressed.erase(mod);
-				});
 				store.call(this, keysStoreKey, pressed);
 			}).delay(0, this); // Fix for IE
 		};
