a23401d8b6d7bf73d3b8ad039f4396f9379c43b0
Arian <arian@aryweb.nl>
2011-04-15 18:36:41 +0200

    Fix Table Specs in node.js and they are using Jasmine now

--- Specs/1.3/Utilities/Table.js
+++ Specs/1.3/Utilities/Table.js
@@ -1,56 +1,55 @@
 /*
-Script: Hash.Cookie.js
-	Specs for Hash.Cookie.js
+Script: Table.js
+	Specs for Table.js
 
 License:
 	MIT-style license.
 */
 
-(function(){
+describe('Table', function(){
+
 	var table = new Table();
 	var one = 1;
 	var obj = {};
+	var fn = function(){};
 
-	describe('Table', {
-
-		'Adds a values to a Table instance': function(){
-			expect(table.length).toEqual(0);
-			table.set('foo', 'bar');
-			expect(table.length).toEqual(1);
-			table.set(one, 'one');
-			table.set(document.body, 'doc body');
-			table.set(obj, 'an object');
-			expect(table.get('foo')).toEqual('bar');
-			expect(table.get(one)).toEqual('one');
-			expect(table.get(document.body)).toEqual('doc body');
-			expect(table.get(obj)).toEqual('an object');
-			expect(table.length).toEqual(4);
-		},
-
-		'Iterates over a Table instance': function(){
-			var keys = [];
-			var values = [];
-			table.each(function(key, val){
-				keys.push(key);
-				values.push(val);
-			});
-			expect(keys).toEqual(['foo', one, document.body, obj]);
-			expect(values).toEqual(['bar', 'one', 'doc body', 'an object']);
-		},
-
-		'Removes values from a Table instance': function(){
-			expect(table.length).toEqual(4);
-			table.erase('foo');
-			expect(table.length).toEqual(3);
-			table.erase(one);
-			table.erase(document.body);
-			table.erase(obj);
-			expect(table.get('foo')).toEqual(null);
-			expect(table.get(one)).toEqual(null);
-			expect(table.get(document.body)).toEqual(null);
-			expect(table.get(obj)).toEqual(null);
-			expect(table.length).toEqual(0);
-		}
-
+	it('Adds a values to a Table instance', function(){
+		expect(table.length).toEqual(0);
+		table.set('foo', 'bar');
+		expect(table.length).toEqual(1);
+		table.set(one, 'one');
+		table.set(fn, 'function');
+		table.set(obj, 'an object');
+		expect(table.get('foo')).toEqual('bar');
+		expect(table.get(one)).toEqual('one');
+		expect(table.get(fn)).toEqual('function');
+		expect(table.get(obj)).toEqual('an object');
+		expect(table.length).toEqual(4);
 	});
-})();
+
+	it('Iterates over a Table instance', function(){
+		var keys = [];
+		var values = [];
+		table.each(function(key, val){
+			keys.push(key);
+			values.push(val);
+		});
+		expect(keys).toEqual(['foo', one, fn, obj]);
+		expect(values).toEqual(['bar', 'one', 'function', 'an object']);
+	});
+
+	it('Removes values from a Table instance', function(){
+		expect(table.length).toEqual(4);
+		table.erase('foo');
+		expect(table.length).toEqual(3);
+		table.erase(one);
+		table.erase(fn);
+		table.erase(obj);
+		expect(table.get('foo')).toEqual(null);
+		expect(table.get(one)).toEqual(null);
+		expect(table.get(fn)).toEqual(null);
+		expect(table.get(obj)).toEqual(null);
+		expect(table.length).toEqual(0);
+	});
+
+});
