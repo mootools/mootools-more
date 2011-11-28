010a56710f260cbe7dc6eed7179e82526dc275de
Werner Mollentze <lists@mollentze.co.za>
2011-11-28 22:11:38 +0200

    Added Afrikaans locale

---
+++ Source/Locale/Locale.af-ZA.Number.js
@@ -0,0 +1,22 @@
+/*
+---
+
+name: Locale.af-ZA.Number
+
+description: Number messages for ZA Afrikaans.
+
+license: MIT-style license
+
+authors:
+  - Werner Mollentze
+
+requires:
+  - /Locale
+  - /Locale.ZA.Number
+
+provides: [Locale.af-ZA.Number]
+
+...
+*/
+
+Locale.define('af-ZA').inherit('ZA', 'Number');
