b117d492350df86edd1d28f0b19a5e03a9fd36fb
megawac <megawac@gmail.com>
2013-08-19 15:49:56 -0400

    Elements.from Optimizations
    Updated Elements.from to use the native parser if available. See
    http://jsperf.com/dom-create-vs-jquery/11

--- Source/Element/Elements.From.js
+++ Source/Element/Elements.From.js
@@ -11,6 +11,7 @@ license: MIT-style license
 
 authors:
   - Aaron Newton
+  - adapted by Graeme Yeates
 
 requires:
   - Core/String
@@ -21,20 +22,40 @@ provides: [Elements.from, Elements.From]
 
 ...
 */
+window.addEvent("domready", function(){
+    function tableFix(match, text) {
+        var container = new Element('table');
+        var tag = match[1].toLowerCase();
+        if (['td', 'th', 'tr'].contains(tag)){
+            container = new Element('tbody').inject(container);
+            if (tag != 'tr') container = new Element('tr').inject(container);
+        }
+        return container.set('html', text).getChildren();
+    }
+    var table_re = /^\s*<(t[dhr]|tbody|tfoot|thead)/i;
+    var range = document.createRange();
+    if(range.createContextualFragment) {
+        var reference = document.getElement("div");
+        range.selectNode(reference);
 
-Elements.from = function(text, excludeScripts){
-	if (excludeScripts || excludeScripts == null) text = text.stripScripts();
+        Elements.from = function(text, excludeScripts) {
+            if (excludeScripts || excludeScripts == null) text = text.stripScripts();
 
-	var container, match = text.match(/^\s*(?:<!--.*?-->\s*)*<(t[dhr]|tbody|tfoot|thead)/i);
+            var match = text.match(table_re);
+            if(match) return tableFix(match,text);
 
-	if (match){
-		container = new Element('table');
-		var tag = match[1].toLowerCase();
-		if (['td', 'th', 'tr'].contains(tag)){
-			container = new Element('tbody').inject(container);
-			if (tag != 'tr') container = new Element('tr').inject(container);
-		}
-	}
+            var elements = range.createContextualFragment(text).childNodes;
+            return new Elements(elements);
+        };
 
-	return (container || new Element('div')).set('html', text).getChildren();
-};
+    } else { //fall back for ie<9
+        Elements.from = function(text, excludeScripts){
+            if (excludeScripts || excludeScripts == null) text = text.stripScripts();
+
+            var match = text.match(table_re);
+            if(match) return tableFix(match,text);
+
+            return new Element('div').set('html', text).getChildren();
+        };
+    }
+});
