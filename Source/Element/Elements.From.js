47fc011276b642c430b67fa51c2ec65ef50c76c6
megawac <megawac@gmail.com>
2013-09-11 20:18:27 -0400

    Elements.from optimization and ie<9 fix
    Uses native createContextualFragment to parse html string if available.
    http://jsperf.com/dom-create-vs-jquery/14
    
    Fixed a bug (https://github.com/mootools/mootools-more/issues/1211) in
    ie <9 where text nodes would not be returned by the function.

--- Source/Element/Elements.From.js
+++ Source/Element/Elements.From.js
@@ -11,6 +11,7 @@ license: MIT-style license
 
 authors:
   - Aaron Newton
+  - adapted by Graeme Yeates
 
 requires:
   - Core/String
@@ -21,20 +22,40 @@ provides: [Elements.from, Elements.From]
 
 ...
 */
+window.addEvent("domready", function(){
+    function getChildren($ele) {return new Elements($ele.childNodes);}//fix for #2527
+    function tableFix(match, text) {
+        var container = new Element('table');
+        var tag = match[1].toLowerCase();
+        if (['td', 'th', 'tr'].contains(tag)){
+            container = new Element('tbody').inject(container);
+            if (tag != 'tr') container = new Element('tr').inject(container);
+        }
+        return getChildren(container.set('html', text));
+    }
+    var table_re = /^\s*<(t[dhr]|tbody|tfoot|thead)/i;
+    var range = document.createRange && document.createRange();
+    if(range && range.createContextualFragment) {
+        var reference = document.getElement("div");
+        range.selectNode(reference);
 
-Elements.from = function(text, excludeScripts){
-	if (excludeScripts || excludeScripts == null) text = text.stripScripts();
+        Elements.from = function(text, excludeScripts) {
+            if (excludeScripts || excludeScripts == null) text = text.stripScripts();
 
-	var container, match = text.match(/^\s*(?:<!--.*?-->\s*)*<(t[dhr]|tbody|tfoot|thead)/i);
+            var match = text.match(table_re);
+            if(match) return tableFix(match,text);
 
-	if (match){
-		container = new Element('table');
-		var tag = match[1].toLowerCase();
-		if (['td', 'th', 'tr'].contains(tag)){
-			container = new Element('tbody').inject(container);
-			if (tag != 'tr') container = new Element('tr').inject(container);
-		}
-	}
+            return getChildren(range.createContextualFragment(text));
+        };
 
-	return (container || new Element('div')).set('html', text).getChildren();
-};
+    } else { //fall back for ie<9
+        Elements.from = function(text, excludeScripts){
+            if (excludeScripts || excludeScripts == null) text = text.stripScripts();
+
+            var match = text.match(table_re);
+            if(match) return tableFix(match,text);
+
+            return getChildren(new Element('div').set('html', text));
+        };
+    }
+});
