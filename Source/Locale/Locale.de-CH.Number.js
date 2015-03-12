3038bfa82134de6600b86b590039b53eac7389cb
Arian Stolwijk <stolwijk.arian@gmail.com>
2015-03-12 11:10:30 +0100

    Fixing YAML headers of the Locale.de-CH.Number module
    - adding it to package.yml
    - using unix newlines

--- Source/Locale/Locale.de-CH.Number.js
+++ Source/Locale/Locale.de-CH.Number.js
@@ -1,15 +1,15 @@
 /*
- ---
- name: Locale.de-CH.Number
- description: Number messages for Switzerland.
- license: MIT-style license
- authors:
- - Kim D. Jeker
- requires:
- - Locale
- - Locale.CH.Number
- provides: [Locale.de-CH.Number]
- ...
- */
+---
+name: Locale.de-CH.Number
+description: Number messages for Switzerland.
+license: MIT-style license
+authors:
+  - Kim D. Jeker
+requires:
+  - Locale
+  - Locale.CH.Number
+provides: [Locale.de-CH.Number]
+...
+*/
 
 Locale.define('de-CH').inherit('CH', 'Number');
