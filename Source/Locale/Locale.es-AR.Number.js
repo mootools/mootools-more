ea31988d4f35280e2596ebcc7287ad629f476bca
Oscar Kuchuk <oscar@localhost.localdomain>
2015-05-01 13:51:51 -0300

    Check with travis

--- Source/Locale/Locale.es-AR.Number.js
+++ Source/Locale/Locale.es-AR.Number.js
@@ -1,31 +1,50 @@
 /*
- * ---
- *  name: Locale.es-AR.Number
- *  description: Number locale for Spanish (Argentina).
- *  license: MIT-style license
- *  authors:
- *    - Oscar Kuchuk
- *    requires:
- *      - Locale
- *      provides: [Locale.es-AR.Number]
- *      check with travis
- *      */
+---
+
+name: Locale.es-AR.Number
+
+description: Number messages for es Argentina.
+
+license: MIT-style license
+
+authors:
+  - Oscar Kuchuk
+
+requires:
+  - Locale
+
+provides: [Locale.es-AR.Number]
+
+...
+*/
 
 Locale.define('es-AR', 'Number', {
 
-    decimal: ',',
-    group: '.',
+	decimal: ',',
+	group: '.',
 
-    negative: {
-        prefix: '-'
-    },
+/* 	Commented properties are the defaults for Number.format
+	decimals: 0,
+	precision: 0,
+	scientific: null,
 
-    currency: {
-        decimals: 2,
-        prefix: '$ '
-    },
-    percentage: {
-        decimals: 2,
-        suffix: '%'
-    }
+	prefix: null,
+	suffic: null,
+
+	// Negative/Currency/percentage will mixin Number
+	negative: {
+		prefix: '-'
+	},*/
+
+	currency: {
+		decimals: 2,
+		prefix: '$ '
+	}/*,
+
+	percentage: {
+		decimals: 2,
+		suffix: '%'
+	}*/
+
 });
+
